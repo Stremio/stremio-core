d5d7dac1444e8cf0088e040402216b43416f9a66
Ivo Georgiev <ivo@strem.io>
2019-01-05 23:00:28 +0200

    initial state type

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use reqwest::*;
 pub mod types;
 use self::types::*;
 
+pub mod state_types;
+use self::state_types::*;
+
 fn get_cinemeta() -> Result<CatalogResponse> {
     Ok(reqwest::get("https://v3-cinemeta.strem.io/catalog/movie/top.json")?.json()?)
 }
@@ -19,6 +22,12 @@ mod tests {
     fn it_works() {
         //let resp = MetaResponse{ metas: vec![] };
         println!("{:?} {:?}", get_cinemeta(), get_watchhub());
+        let state = State{
+            catalog: Loadable::Ready(CatalogView::Content(CatalogContent{
+                items: get_cinemeta().expect("rip").metas
+            }))
+        };
+        println!("{:?}", state);
         assert_eq!(2 + 2, 4);
     }
 }
--- src/state_types/mod.rs
+++ src/state_types/mod.rs
@@ -1,7 +1,31 @@
-enum Action { Init }
+use crate::types::*;
 
-enum Loadable<T> {
+#[derive(Debug)]
+pub enum Action { Init }
+
+#[derive(Debug)]
+pub enum Loadable<T> {
     NotLoaded,
     Loading,
     Ready(T)
+}
+
+// @TODO: can we do this with less generics and less nested enums
+
+#[derive(Debug)]
+pub struct CatalogContent<T> {
+    // @TODO filters
+    //filters: Vec<>
+    pub items: Vec<T>
+}
+// @TODO: more thinking on this; message should be more complicated
+#[derive(Debug)]
+pub enum CatalogView<T> {
+    Message(String),
+    Content(CatalogContent<T>),
+}
+
+#[derive(Debug)]
+pub struct State {
+    pub catalog: Loadable<CatalogView<MetaItem>>,
 }
