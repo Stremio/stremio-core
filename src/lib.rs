347b48ec7cef7379adf5c4a07090e6306a4abc8c
Ivo Georgiev <ivo@strem.io>
2019-01-05 22:27:18 +0200

    reqwest and deserializing meta responses

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
-mod types;
-use types::meta_item::MetaItem;
+extern crate reqwest;
+use reqwest::*;
+
+pub mod types;
+use types::*;
+
+//fn get_cinemeta() -> Result<MetaResponse> {
+//    reqwest::get("https://v3-cinemeta.strem.io/catalog/movies/top.json")?.json()?
+//}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let meta = MetaItem{ id: "test".to_string(), item_type: "series".to_string(), name: "foobar".to_string() };
-        println!("{:?}", meta);
+        let resp = MetaResponse{ metas: vec![] };
+        println!("{:?}", resp);
         assert_eq!(2 + 2, 4);
     }
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,1 +1,9 @@
-pub mod meta_item;
+use serde_derive::*;
+
+mod meta_item;
+pub use meta_item::*;
+
+#[derive(Deserialize, Debug)]
+pub struct MetaResponse {
+    pub metas: Vec<MetaItem>,
+}
