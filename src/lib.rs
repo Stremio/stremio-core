a50b29dd5bb94b57da93b734806000e165746dc9
Ivo Georgiev <ivo@strem.io>
2019-01-06 20:03:34 +0200

    refined tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,12 @@
-extern crate reqwest;
-use reqwest::{Result,get};
-use serde_json::to_string;
-
 pub mod types;
 use self::types::*;
 
 pub mod state_types;
 use self::state_types::*;
-
-fn get_cinemeta() -> reqwest::Result<CatalogResponse> {
-    Ok(reqwest::get("https://v3-cinemeta.strem.io/catalog/movie/top.json")?.json()?)
-}
-
-fn get_watchhub() -> reqwest::Result<StreamResponse> {
-    Ok(reqwest::get("https://watchhub-us.strem.io/stream/movie/tt0120338.json")?.json()?)
-}
-
 #[cfg(test)]
 mod tests {
+    use serde_json::to_string;
+    use reqwest::{Result,get};
     use super::*;
     #[test]
     fn it_works() {
@@ -43,8 +32,24 @@ mod tests {
             Ok(resp) => { Action::CatalogsReceived(Ok(resp)) },
             Err(err) => { Action::CatalogsReceived(Err("request error")) },
         });
-        println!("{:?}", &container.state);
+        assert_eq!(
+            match &container.state.catalog {
+                Loadable::Ready(ItemsView::Grouped(x)) => x.len(),
+                _ => 100,
+            },
+            100,
+        );
+        //println!("{:?}", &container.state);
         //println!("{}", serde_json::to_string(&state).expect("rip"));
         //assert_eq!(2 + 2, 4);
+    }
+
+
+    fn get_cinemeta() -> reqwest::Result<CatalogResponse> {
+        Ok(reqwest::get("https://v3-cinemeta.strem.io/catalog/movie/top.json")?.json()?)
+    }
+
+    fn get_watchhub() -> reqwest::Result<StreamResponse> {
+        Ok(reqwest::get("https://watchhub-us.strem.io/stream/movie/tt0120338.json")?.json()?)
     }
 }
