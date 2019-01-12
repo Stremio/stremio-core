9fb9fdc750f6020af6890c7a7429724a81097047
Ivo Georgiev <ivo@strem.io>
2019-01-13 01:08:36 +0200

    try implementing environment, remove some async stuff that are not needed anymore

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,8 @@ mod tests {
     use serde_json::to_string;
     use reqwest::{Result,get};
     use super::*;
-    use futures::Future;
-    use futures::future;
-
+    use futures::{Future,future};
+    use std::error::Error;
 
 #[test]
     fn it_works() {
@@ -19,35 +18,25 @@ mod tests {
         // -> UserMiddleware -> CatalogMiddleware -> DetailMiddleware -> AddonsMiddleware ->
         // PlayerMiddleware -> LibNotifMiddleware -> join(discoverContainer, boardContainer, ...)
         let mut container = Container::with_reducer(CatalogGrouped::empty(), &catalogs_reducer);
-        let fut = get_addons_async("https://api.strem.io/addonsofficialcollection.json")
-            .and_then(|addons_resp| {
-                 for addon in addons_resp.iter() {
-                    for cat in addon.manifest.catalogs.iter() {
-                        container.dispatch(&match get_catalogs(&addon, &cat.type_name, &cat.id) {
-                            Ok(resp) => { Action::CatalogReceived(Ok(resp)) },
-                            Err(_) => { Action::CatalogReceived(Err(())) },
-                        });
-                    }
-                }
-                // @TODO figure out how to do middlewares/reducers pipeline
-                assert_eq!(container.get_state().groups.len(), 8);
-
-                // @TODO move this out; testing is_supported
-                let cinemeta_m = &addons_resp[0].manifest;
-                assert_eq!(cinemeta_m.is_supported("meta".to_string(), "movie".to_string(), "tt0234".to_string()), true);
-                assert_eq!(cinemeta_m.is_supported("meta".to_string(), "movie".to_string(), "somethingElse".to_string()), false);
-                assert_eq!(cinemeta_m.is_supported("stream".to_string(), "movie".to_string(), "tt0234".to_string()), false);
-                future::ok(())
-            });
-        fut.wait().expect("get_addons_async ran fine");
-    }
-
-    fn get_addons_async(url: &'static str) -> impl Future<Item=Vec<AddonDescriptor>, Error=()> {
-        match get_addons(url) {
-            Err(_) => future::err(()),
-            Ok(e) => future::ok(e),
+        let addons_resp = get_addons("https://api.strem.io/addonsofficialcollection.json").unwrap();
+        for addon in addons_resp.iter() {
+            for cat in addon.manifest.catalogs.iter() {
+                container.dispatch(&match get_catalogs(&addon, &cat.type_name, &cat.id) {
+                    Ok(resp) => { Action::CatalogReceived(Ok(resp)) },
+                    Err(_) => { Action::CatalogReceived(Err(())) },
+                });
+            }
         }
+        // @TODO figure out how to do middlewares/reducers pipeline
+        assert_eq!(container.get_state().groups.len(), 8);
+
+        // @TODO move this out; testing is_supported
+        let cinemeta_m = &addons_resp[0].manifest;
+        assert_eq!(cinemeta_m.is_supported("meta".to_string(), "movie".to_string(), "tt0234".to_string()), true);
+        assert_eq!(cinemeta_m.is_supported("meta".to_string(), "movie".to_string(), "somethingElse".to_string()), false);
+        assert_eq!(cinemeta_m.is_supported("stream".to_string(), "movie".to_string(), "tt0234".to_string()), false);
     }
+
     fn get_addons(url: &'static str) -> reqwest::Result<Vec<AddonDescriptor>> {
         Ok(reqwest::get(url)?.json()?)
     }
@@ -62,6 +51,7 @@ mod tests {
         t_middlewares();
     }
     fn t_middlewares() {
+        // @TODO move this
         struct UserMiddleware{
             id: usize,
             user: Option<String>,
