40c40c7b1b341d63d695d75713251b2cf77ed71e
Ivo Georgiev <ivo@strem.io>
2019-01-13 02:11:25 +0200

    state_types/mod: cleanup the environment thing

--- src/state_types/mod.rs
+++ src/state_types/mod.rs
@@ -11,13 +11,3 @@ pub use self::actions::*;
 
 mod middleware;
 pub use self::middleware::*;
-
-use futures::Future;
-use std::error::Error;
-pub trait Environment {
-    // @TODO: finalize this trait
-    // @TODO: we should use `impl Trait` once it's allowed: https://github.com/rust-lang/rust/issues/34511
-    fn fetch(url: String) -> Box<dyn Future<Item=Vec<u8>, Error=Error>>;
-    //fn storage_get
-    //fn storage_set
-}
