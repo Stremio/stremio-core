b8a4c736f7323e1746fc39300edf00d3d338d456
Ivo Georgiev <ivo@strem.io>
2019-02-06 22:53:42 +0200

    src/middlewares/addons: description() is soft deprecated

--- src/middlewares/addons.rs
+++ src/middlewares/addons.rs
@@ -26,7 +26,7 @@ impl<T: Environment> AddonsMiddleware<T> {
         let fut = T::fetch_serde::<_, CatalogResponse>(req).then(move |res| {
             emit(&match res {
                 Ok(resp) => Action::AddonResponse(resource_req, Ok(*resp)),
-                Err(e) => Action::AddonResponse(resource_req, Err(e.description().to_owned())),
+                Err(e) => Action::AddonResponse(resource_req, Err(e.to_string())),
             });
             future::ok(())
         });
