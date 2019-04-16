134b469b85d108df81399650c5971f50da0d37ae
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-16 09:58:26 +0300

    wasm-product renamed to wasm_build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-wasm-pack build --no-typescript --out-dir wasm-product --release --target web
-mv ./wasm-product/state_container_web_bg.wasm ./static/state_container_web.wasm
+wasm-pack build --no-typescript --out-dir wasm_build --release --target web
+mv ./wasm_build/state_container_web_bg.wasm ./static/state_container_web.wasm
 ./node_modules/.bin/babel src/lib.js --out-file build/lib.js --presets=@babel/preset-env
-./node_modules/.bin/babel wasm-product/state_container_web.js --out-file build/state_container_web.js --presets=@babel/preset-env
+./node_modules/.bin/babel wasm_build/state_container_web.js --out-file build/state_container_web.js --presets=@babel/preset-env
