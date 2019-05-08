c71245cfe36de1902aa004d6587aa7149e2e2813
Ivo Georgiev <ivo@strem.io>
2019-05-08 12:30:40 +0300

    ResourceResponse: add LibItems

--- src/types/addons/mod.rs
+++ src/types/addons/mod.rs
@@ -3,8 +3,7 @@ mod manifest;
 mod resource_ref;
 pub use self::manifest::*;
 pub use self::resource_ref::*;
-use crate::types::meta_item::*;
-use crate::types::stream::*;
+use crate::types::{LibItem, MetaPreview, MetaDetail, Stream};
 mod manifest_tests;
 
 pub type TransportUrl = String;
@@ -34,6 +33,9 @@ pub enum ResourceResponse {
         // fallback to all of the previous variants, therefore resulting in inaccurate err messages
         // To support other /meta/ responses (meta extensions), we should make a MetaExt variant
         meta: MetaDetail,
+    },
+    LibItems {
+        libitems: Vec<LibItem>,
     },
     Streams {
         streams: Vec<Stream>,
