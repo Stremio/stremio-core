914b64ff5324e6118f861793235507b317a3f97b
Ivo Georgiev <ivo@strem.io>
2019-06-08 15:55:20 +0300

    effects type

--- src/state_types/mod.rs
+++ src/state_types/mod.rs
@@ -1,17 +1,21 @@
+mod environment;
+pub use self::environment::*;
+
+mod msg;
+pub use self::msg::*;
+
+mod effects;
+pub use self::effects::*;
+
+// @TODO everything underneath will be dropped with the Elm architecture rewrite
 mod container;
 pub use self::container::*;
 
 mod catalogs;
 pub use self::catalogs::*;
 
-mod msg;
-pub use self::msg::*;
-
 mod chain;
 pub use self::chain::*;
-
-mod environment;
-pub use self::environment::*;
 
 mod container_muxer;
 pub use self::container_muxer::*;
