526a53ac61825cfbd81c52dfbf1e36084e722def
Ivo Georgiev <ivo@strem.io>
2019-07-27 19:28:32 +0300

    Notifications WIP

--- src/state_types/models/lib_recent.rs
+++ src/state_types/models/lib_recent.rs
@@ -30,3 +30,52 @@ impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for LibRecent {
         }
     }
 }
+
+use crate::types::Video;
+//use lazysort::SortedBy;
+#[derive(Debug, Clone, Default, Serialize)]
+pub struct Notifications {
+    pub groups: Vec<ItemsGroup<Vec<Video>>>,
+}
+impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for Notifications {
+    fn update(&mut self, ctx: &Ctx<Env>, msg: &Msg) -> Effects {
+        match msg {
+            Msg::Action(Action::Load(ActionLoad::Notifications)) => {
+                let lib = match &ctx.library {
+                    LibraryLoadable::Ready(l) => l,
+                    _ => {
+                        self.groups = vec![];
+                        return Effects::none();
+                    }
+                };
+
+                // @TODO group into groups of 50, by type and maybe ID prefix?
+                let relevant_items = &lib
+                    .items
+                    .values()
+                    .filter(|item| item.type_name == "series" && !item.state.no_notif && !item.removed)
+                    .sorted_by(|a, b| b.mtime.cmp(&a.mtime))
+                    .take(50)
+                    .collect::<Vec<_>>();
+                /*
+                let (groups, effects) = addon_aggr_new::<Env, _>(
+                    &ctx.content.addons,
+                    &AggrRequest::AllOfCatalog { extra },
+                );
+                self.groups = groups;
+                effects
+                */
+                /*
+                let ids = vec!["tt7366338".to_string(), "tt2306299".to_string()];
+                let path = ResourceRef::with_extra("catalog", "series", "last-videos", &[( "lastVideosIds".into(), ids.join(",") )]);
+                dbg!(path.to_string());
+                */
+    
+                dbg!(&relevant_items);
+                // @TODO fetch groups
+                Effects::none()
+            },
+            _ => Effects::none().unchanged(),
+        }
+    }
+}
