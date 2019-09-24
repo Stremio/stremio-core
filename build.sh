e8db182ec501c223219d6a03969f18320330d816
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-09-24 23:10:49 +0300

    lib.js dropped

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -ex
 wasm-pack build --no-typescript --out-dir wasm_build --release --target web
-mv ./wasm_build/stremio_core_web_bg.wasm ./static/stremio_core_web.wasm
+mv ./wasm_build/stremio_core_web_bg.wasm ./static/stremio_core_web_bg.wasm
 npx babel wasm_build/stremio_core_web.js --config-file ./.babelrc --out-file build/stremio_core_web.js
-npx babel src/lib.js --config-file ./.babelrc --out-file build/lib.js
