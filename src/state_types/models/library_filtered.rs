7231e60c030a2c9e64ad50721a9a37c54d2811f3
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-11-15 11:24:29 +0200

    LibraryFiltered updated when lib changed

--- src/state_types/models/library_filtered.rs
+++ src/state_types/models/library_filtered.rs
@@ -1,33 +1,40 @@
 use super::{Ctx, LibraryLoadable};
 use crate::state_types::*;
-use crate::types::LibItem;
+use crate::types::{LibBucket, LibItem};
 use itertools::Itertools;
 use serde_derive::*;
 
 #[derive(Debug, Clone, Default, Serialize)]
 pub struct LibraryFiltered {
-    pub items: Vec<LibItem>,
+    pub selected: Option<String>,
     pub types: Vec<String>,
+    pub items: Vec<LibItem>,
 }
+
 impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for LibraryFiltered {
     fn update(&mut self, ctx: &Ctx<Env>, msg: &Msg) -> Effects {
         match msg {
-            Msg::Action(Action::Load(ActionLoad::LibItemsByType(item_type))) => {
-                if let LibraryLoadable::Ready(l) = &ctx.library {
-                    self.items = l
-                        .items
-                        .values()
-                        .filter(|x| x.type_name == *item_type && !x.removed)
-                        .sorted_by(|a, b| b.mtime.cmp(&a.mtime))
-                        .cloned()
-                        .collect();
-                    self.types = l
-                        .items
-                        .values()
-                        .filter(|x| !x.removed)
-                        .map(|x| x.type_name.to_owned())
-                        .unique()
-                        .collect();
+            Msg::Action(Action::Load(ActionLoad::LibItemsByType(type_name))) => {
+                self.selected = Some(type_name.to_owned());
+                if let LibraryLoadable::Ready(bucket) = &ctx.library {
+                    self.types = get_types(bucket);
+                    self.items = get_items(bucket, type_name);
+                };
+                Effects::none()
+            }
+            Msg::Internal(Internal::LibLoaded(bucket)) => {
+                self.types = get_types(bucket);
+                if let Some(selected) = &self.selected {
+                    self.items = get_items(bucket, selected);
+                };
+                Effects::none()
+            }
+            Msg::Event(Event::CtxChanged) | Msg::Event(Event::LibPersisted) => {
+                if let LibraryLoadable::Ready(bucket) = &ctx.library {
+                    self.types = get_types(bucket);
+                    if let Some(selected) = &self.selected {
+                        self.items = get_items(bucket, selected);
+                    };
                     Effects::none()
                 } else {
                     Effects::none().unchanged()
@@ -36,4 +43,23 @@ impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for LibraryFiltered {
             _ => Effects::none().unchanged(),
         }
     }
+}
+
+fn get_types(bucket: &LibBucket) -> Vec<String> {
+    bucket
+        .items
+        .values()
+        .filter(|x| !x.removed)
+        .map(|x| x.type_name.to_owned())
+        .unique()
+        .collect()
+}
+
+fn get_items(bucket: &LibBucket, type_name: &String) -> Vec<LibItem> {
+    bucket
+        .items
+        .values()
+        .filter(|item| !item.removed && item.type_name.eq(type_name))
+        .cloned()
+        .collect()
 }
