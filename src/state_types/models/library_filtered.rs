ebf2674bfba421cb99aed7a9e90c8bf28bd9bd79
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-11-15 11:55:35 +0200

    reset libitems if libbucket not loaded

--- src/state_types/models/library_filtered.rs
+++ src/state_types/models/library_filtered.rs
@@ -16,50 +16,57 @@ impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for LibraryFiltered {
         match msg {
             Msg::Action(Action::Load(ActionLoad::LibItemsByType(type_name))) => {
                 self.selected = Some(type_name.to_owned());
-                if let LibraryLoadable::Ready(bucket) = &ctx.library {
-                    self.types = get_types(bucket);
-                    self.items = get_items(bucket, type_name);
+                let bucket = match &ctx.library {
+                    LibraryLoadable::Ready(bucket) => Some(bucket),
+                    _ => None,
                 };
+                self.types = get_types(bucket);
+                self.items = get_items(bucket, Some(type_name));
                 Effects::none()
             }
             Msg::Internal(Internal::LibLoaded(bucket)) => {
-                self.types = get_types(bucket);
-                if let Some(selected) = &self.selected {
-                    self.items = get_items(bucket, selected);
-                };
+                self.types = get_types(Some(bucket));
+                self.items = get_items(Some(bucket), self.selected.as_ref());
                 Effects::none()
             }
             Msg::Event(Event::CtxChanged) | Msg::Event(Event::LibPersisted) => {
-                if let LibraryLoadable::Ready(bucket) = &ctx.library {
-                    self.types = get_types(bucket);
-                    if let Some(selected) = &self.selected {
-                        self.items = get_items(bucket, selected);
-                    };
-                    Effects::none()
-                } else {
-                    Effects::none().unchanged()
-                }
+                let bucket = match &ctx.library {
+                    LibraryLoadable::Ready(bucket) => Some(bucket),
+                    _ => None,
+                };
+                self.types = get_types(bucket);
+                self.items = get_items(bucket, self.selected.as_ref());
+                Effects::none()
             }
             _ => Effects::none().unchanged(),
         }
     }
 }
 
-fn get_types(bucket: &LibBucket) -> Vec<String> {
-    bucket
-        .items
-        .values()
-        .filter(|x| !x.removed)
-        .map(|x| x.type_name.to_owned())
-        .unique()
-        .collect()
+fn get_types(bucket: Option<&LibBucket>) -> Vec<String> {
+    if let Some(bucket) = bucket {
+        bucket
+            .items
+            .values()
+            .filter(|x| !x.removed)
+            .map(|x| x.type_name.to_owned())
+            .unique()
+            .collect()
+    } else {
+        Vec::new()
+    }
 }
 
-fn get_items(bucket: &LibBucket, type_name: &String) -> Vec<LibItem> {
-    bucket
-        .items
-        .values()
-        .filter(|item| !item.removed && item.type_name.eq(type_name))
-        .cloned()
-        .collect()
+fn get_items(bucket: Option<&LibBucket>, type_name: Option<&String>) -> Vec<LibItem> {
+    if bucket.is_some() && type_name.is_some() {
+        bucket
+            .unwrap()
+            .items
+            .values()
+            .filter(|item| !item.removed && item.type_name.eq(type_name.unwrap()))
+            .cloned()
+            .collect()
+    } else {
+        Vec::new()
+    }
 }
