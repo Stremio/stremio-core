acb40147f4c63395082c1bebd8ec6a0bdc54a8cd
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-11-27 13:25:55 +0200

    internal crate created

---
+++ src/state_types/msg/internal.rs
@@ -0,0 +1,25 @@
+use crate::state_types::{CtxContent, EnvError, SsSettings};
+use crate::types::addons::{Descriptor, ResourceRequest, ResourceResponse};
+use crate::types::api::AuthKey;
+use crate::types::LibBucket;
+
+#[derive(Debug)]
+pub enum Internal {
+    // Context loaded from storage
+    CtxLoaded(Option<Box<CtxContent>>),
+    // Context updated as a result of authentication/logout
+    CtxUpdate(Box<CtxContent>),
+    // Context addons pulled
+    // this should replace ctx.content.addons entirely
+    CtxAddonsPulled(AuthKey, Vec<Descriptor>),
+    // Library is loaded, either from storage or from an initial API sync
+    // This will replace the whole library index, as long as bucket UID matches
+    LibLoaded(LibBucket),
+    // Library: pulled some newer items from the API
+    // this will extend the current index of libitems, it doesn't replace it
+    LibSyncPulled(LibBucket),
+    // Response from an add-on
+    AddonResponse(ResourceRequest, Box<Result<ResourceResponse, EnvError>>),
+    StreamingServerSettingsLoaded(SsSettings),
+    StreamingServerSettingsErrored(String),
+}
--- src/state_types/msg/mod.rs
+++ src/state_types/msg/mod.rs
@@ -12,26 +12,9 @@ pub use actions::*;
 // Intermediery messages
 // those are emitted by the middlewares and received by containers
 //
-#[derive(Debug)]
-pub enum Internal {
-    // Context loaded from storage
-    CtxLoaded(Option<Box<CtxContent>>),
-    // Context updated as a result of authentication/logout
-    CtxUpdate(Box<CtxContent>),
-    // Context addons pulled
-    // this should replace ctx.content.addons entirely
-    CtxAddonsPulled(AuthKey, Vec<Descriptor>),
-    // Library is loaded, either from storage or from an initial API sync
-    // This will replace the whole library index, as long as bucket UID matches
-    LibLoaded(LibBucket),
-    // Library: pulled some newer items from the API
-    // this will extend the current index of libitems, it doesn't replace it
-    LibSyncPulled(LibBucket),
-    // Response from an add-on
-    AddonResponse(ResourceRequest, Box<Result<ResourceResponse, EnvError>>),
-    StreamingServerSettingsLoaded(SsSettings),
-    StreamingServerSettingsErrored(String),
-}
+
+pub mod internal;
+pub use internal::*;
 
 //
 // Event
