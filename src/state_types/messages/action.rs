acff6323f65dc4e89cf9df41cd7282abd0027d2e
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-11-28 11:57:22 +0200

    CatalogsGrouped load action renamed

--- src/state_types/messages/action.rs
+++ src/state_types/messages/action.rs
@@ -7,7 +7,7 @@ use serde_derive::{Deserialize, Serialize};
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 #[serde(tag = "load", content = "args")]
 pub enum ActionLoad {
-    CatalogGrouped {
+    CatalogsGrouped {
         extra: Vec<ExtraProp>,
     },
     CatalogFiltered(ResourceRequest),
