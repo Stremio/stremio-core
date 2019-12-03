e91fb0a6cfcd3cc7d4f524f1123d3bc05255565e
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-12-03 16:54:07 +0200

    lifetime removed from constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const SKIP: &'static str = "skip";
-pub const SEARCH: &'static str = "search";
+pub const SKIP: &str = "skip";
+pub const SEARCH: &str = "search";
 pub const CATALOG_PAGE_SIZE: usize = 100;
 pub const CATALOG_PREVIEW_SIZE: usize = 10;
