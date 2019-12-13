1cda4f03a9535d9320f15f9cb6fe41580562a396
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-12-13 17:04:08 +0200

    OFFICIAL_ADDONS moved to constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,15 @@
+use crate::types::addons::Descriptor;
+use lazy_static::lazy_static;
+
 pub const SKIP_EXTRA_NAME: &str = "skip";
 pub const SEARCH_EXTRA_NAME: &str = "search";
 pub const META_RESOURCE_NAME: &str = "meta";
 pub const STREAM_RESOURCE_NAME: &str = "stream";
 pub const META_CATALOG_PAGE_SIZE: usize = 100;
 pub const META_CATALOG_PREVIEW_SIZE: usize = 10;
+
+lazy_static! {
+    pub static ref OFFICIAL_ADDONS: Vec<Descriptor> =
+        serde_json::from_slice(include_bytes!("../stremio-official-addons/index.json"))
+            .expect("official addons JSON parse");
+}
--- src/state_types/models/ctx.rs
+++ src/state_types/models/ctx.rs
@@ -1,3 +1,4 @@
+use crate::constants::OFFICIAL_ADDONS;
 use crate::state_types::messages::Event::*;
 use crate::state_types::messages::Internal::*;
 use crate::state_types::messages::*;
@@ -7,17 +8,10 @@ use crate::types::addons::Descriptor;
 use crate::types::api::*;
 use derivative::*;
 use futures::Future;
-use lazy_static::*;
 use serde_derive::*;
 use std::marker::PhantomData;
 
 const USER_DATA_KEY: &str = "userData";
-lazy_static! {
-    static ref DEFAULT_ADDONS: Vec<Descriptor> = serde_json::from_slice(include_bytes!(
-        "../../../stremio-official-addons/index.json"
-    ))
-    .expect("official addons JSON parse");
-}
 
 // These will be stored, so they need to implement both Serialize and Deserilaize
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
@@ -36,7 +30,7 @@ impl Default for CtxContent {
     fn default() -> Self {
         CtxContent {
             auth: None,
-            addons: DEFAULT_ADDONS.to_owned(),
+            addons: OFFICIAL_ADDONS.to_owned(),
             settings: Settings::default(),
         }
     }
@@ -140,9 +134,9 @@ impl<Env: Environment + 'static> Update for Ctx<Env> {
                         Effects::one(Box::new(ft)).unchanged()
                     }
                     None => {
-                        // Local update based on the DEFAULT_ADDONS
+                        // Local update based on the OFFICIAL_ADDONS
                         self.content.addons =
-                            addons_upgrade_local(&DEFAULT_ADDONS, &self.content.addons);
+                            addons_upgrade_local(&OFFICIAL_ADDONS, &self.content.addons);
                         Effects::none()
                     }
                 },
