849c1450e3d76251d20679b293516bfccfd33d9e
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-12-16 11:12:18 +0200

    use installed addon descriptor in addon details

--- src/state_types/models/addon_details.rs
+++ src/state_types/models/addon_details.rs
@@ -1,5 +1,7 @@
 use crate::state_types::messages::{Action, ActionLoad, Internal, Msg};
-use crate::state_types::models::common::{descriptor_update, DescriptorAction, DescriptorLoadable};
+use crate::state_types::models::common::{
+    descriptor_update, DescriptorAction, DescriptorContent, DescriptorLoadable,
+};
 use crate::state_types::models::Ctx;
 use crate::state_types::{Effects, Environment, UpdateWithCtx};
 use serde::Serialize;
@@ -10,13 +12,29 @@ pub struct AddonDetails {
 }
 
 impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for AddonDetails {
-    fn update(&mut self, _ctx: &Ctx<Env>, msg: &Msg) -> Effects {
+    fn update(&mut self, ctx: &Ctx<Env>, msg: &Msg) -> Effects {
         match msg {
             Msg::Action(Action::Load(ActionLoad::AddonDetails { transport_url })) => {
-                descriptor_update::<Env>(
-                    &mut self.descriptor,
-                    DescriptorAction::DescriptorRequested { transport_url },
-                )
+                let descriptor = ctx
+                    .content
+                    .addons
+                    .iter()
+                    .find(|addon| addon.transport_url.eq(transport_url));
+                match descriptor {
+                    Some(descriptor) => descriptor_update::<Env>(
+                        &mut self.descriptor,
+                        DescriptorAction::DescriptorReplaced {
+                            descriptor: Some(DescriptorLoadable {
+                                transport_url: transport_url.to_owned(),
+                                content: DescriptorContent::Ready(descriptor.to_owned()),
+                            }),
+                        },
+                    ),
+                    None => descriptor_update::<Env>(
+                        &mut self.descriptor,
+                        DescriptorAction::DescriptorRequested { transport_url },
+                    ),
+                }
             }
             Msg::Action(Action::Unload) => descriptor_update::<Env>(
                 &mut self.descriptor,
