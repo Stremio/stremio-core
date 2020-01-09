911f993dd55ff0b83af2d1f8d3167b4e087900a3
nklhrstv <me@nikolahristov.com>
2020-01-09 23:04:32 +0200

    impl Error for MsgError

--- src/state_types/messages/error.rs
+++ src/state_types/messages/error.rs
@@ -1,12 +1,30 @@
 use crate::state_types::EnvError;
 use crate::types::api::APIErr;
 use serde::Serialize;
+use std::error::Error;
+use std::fmt;
 
 #[derive(Debug, Clone, Serialize)]
 #[serde(tag = "type", content = "args")]
 pub enum MsgError {
     API { message: String, code: u64 },
     Env { message: String },
+}
+
+impl fmt::Display for MsgError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let message = match &self {
+            MsgError::API { message, .. } => message.to_owned(),
+            MsgError::Env { message } => message.to_owned(),
+        };
+        write!(f, "{}", message)
+    }
+}
+
+impl Error for MsgError {
+    fn description(&self) -> &str {
+        format!("{}", &self).as_str()
+    }
 }
 
 impl From<APIErr> for MsgError {
