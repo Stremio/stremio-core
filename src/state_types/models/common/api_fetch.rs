da4d550b8a66f4e2b0456676d24de9ac5a8a25ea
nklhrstv <me@nikolahristov.com>
2020-01-09 13:29:46 +0200

    api-fetch moved in state_types

--- src/state_types/models/common/api_fetch.rs
+++ src/state_types/models/common/api_fetch.rs
@@ -1,13 +1,15 @@
-use super::{APIMethodName, APIResult};
-use crate::state_types::messages::CtxError;
+use crate::state_types::messages::MsgError;
 use crate::state_types::{Environment, Request};
+use crate::types::api::{APIMethodName, APIResult};
 use futures::{future, Future};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 
-pub fn api_fetch<Env, OUT, REQ>(req: REQ) -> impl Future<Item = OUT, Error = CtxError>
+pub fn api_fetch<Env, OUT, REQ>(req: REQ) -> impl Future<Item = OUT, Error = MsgError>
 where
     Env: Environment,
-    OUT: serde::de::DeserializeOwned + 'static,
-    REQ: APIMethodName + serde::Serialize + 'static,
+    OUT: DeserializeOwned + 'static,
+    REQ: APIMethodName + Serialize + 'static,
 {
     let url = format!("{}/api/{}", Env::api_url(), req.method_name());
     let req = Request::post(url).body(req).expect("builder cannot fail");
