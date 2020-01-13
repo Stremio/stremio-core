5972afaa87480ef78c91ac4992cc42286aa66165
nklhrstv <me@nikolahristov.com>
2020-01-13 14:43:28 +0200

    models modules declared public

--- src/state_types/models/mod.rs
+++ src/state_types/models/mod.rs
@@ -1,31 +1,31 @@
 pub mod common;
 
-mod addon_details;
+pub mod addon_details;
 pub use addon_details::*;
 
-mod catalog_filtered;
+pub mod catalog_filtered;
 pub use catalog_filtered::*;
 
-mod catalogs_with_extra;
+pub mod catalogs_with_extra;
 pub use catalogs_with_extra::*;
 
-mod continue_watching;
+pub mod continue_watching;
 pub use continue_watching::*;
 
 pub mod ctx;
 pub use ctx::*;
 
-mod library_filtered;
+pub mod library_filtered;
 pub use library_filtered::*;
 
-mod meta_details;
+pub mod meta_details;
 pub use meta_details::*;
 
-mod notifications;
+pub mod notifications;
 pub use notifications::*;
 
-mod player;
+pub mod player;
 pub use player::*;
 
-mod settings;
+pub mod settings;
 pub use settings::*;
