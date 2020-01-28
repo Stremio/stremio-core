5f59ea93b8ab8b631ed5f6a38aa2fb8204a4cabc
nklhrstv <me@nikolahristov.com>
2020-01-28 15:29:55 +0200

    basic streaming server implemented

--- src/state_types/models/ctx/streaming_server.rs
+++ src/state_types/models/ctx/streaming_server.rs
@@ -1,4 +1,6 @@
+use crate::constants::STREAMING_SERVER_URL;
 use crate::state_types::models::common::ModelError;
+use crate::state_types::models::ctx::user_data::UserDataLoadable;
 use crate::state_types::msg::{Action, ActionSettings, Event, Internal, Msg};
 use crate::state_types::{Effects, EnvError, Environment, Request, UpdateWithCtx};
 use derivative::Derivative;
@@ -15,19 +17,29 @@ pub struct Settings {}
 #[derive(Debug, Clone, PartialEq, Serialize)]
 #[serde(tag = "type")]
 pub enum StreamingServerLoadable {
+    Offline {
+        #[serde(with = "url_serde")]
+        url: Url,
+    },
     Loading {
         #[serde(with = "url_serde")]
         url: Url,
     },
-    Error {
-        #[serde(with = "url_serde")]
-        url: Url,
-    },
-    Ready {
+    Online {
         settings: Settings,
         #[serde(with = "url_serde")]
         base_url: Url,
         #[serde(with = "url_serde")]
         url: Url,
     },
+}
+
+impl StreamingServerLoadable {
+    pub fn update<Env: Environment + 'static>(
+        &mut self,
+        user_data: &UserDataLoadable,
+        msg: &Msg,
+    ) -> Effects {
+        Effects::none().unchanged()
+    }
 }
