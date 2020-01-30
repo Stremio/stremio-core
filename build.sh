a9670ac1ec25510d43630cf74e19698330691059
nklhrstv <me@nikolahristov.com>
2020-01-30 14:50:19 +0200

    crate build and static directories

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -ex
 wasm-pack build --no-typescript --out-dir wasm_build --release --target web
+mkdir -p ./static
+mkdir -p ./build
 mv ./wasm_build/stremio_core_web_bg.wasm ./static/stremio_core_web_bg.wasm
 npx babel wasm_build/stremio_core_web.js --config-file ./.babelrc --out-file build/stremio_core_web.js
