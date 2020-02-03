259f9d4de1ce9e10f177455bb1e5e555c253e18b
nklhrstv <me@nikolahristov.com>
2020-02-03 17:15:19 +0200

    cargo fmt

--- src/state_types/models/ctx/ctx.rs
+++ src/state_types/models/ctx/ctx.rs
@@ -260,11 +260,10 @@ impl<Env: Environment + 'static> Update for Ctx<Env> {
                     (Some(auth), LibraryLoadable::Ready(bucket)) => {
                         let uid = bucket.uid.to_owned();
                         Effects::one(Box::new(
-                            LibraryLoadable::sync_with_api::<Env>(&auth.key, bucket.to_owned()).then(
-                                move |result| {
+                            LibraryLoadable::sync_with_api::<Env>(&auth.key, bucket.to_owned())
+                                .then(move |result| {
                                     Ok(Msg::Internal(Internal::LibrarySyncResult(uid, result)))
-                                },
-                            ),
+                                }),
                         ))
                         .unchanged()
                     }
