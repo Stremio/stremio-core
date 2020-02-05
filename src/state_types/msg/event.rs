ece207cf1c9e9b2272c9f825e7876925dda8d738
nklhrstv <me@nikolahristov.com>
2020-02-05 18:56:14 +0200

    StreamingServerSettingsUpdated event added

--- src/state_types/msg/event.rs
+++ src/state_types/msg/event.rs
@@ -3,6 +3,7 @@ use crate::types::addons::Descriptor;
 use crate::types::api::AuthRequest;
 use crate::types::UID;
 use serde::Serialize;
+use url::Url;
 
 //
 // Those messages are meant to be dispatched by the stremio-core crate and hanled by the users of the stremio-core crate and by the stremio-core crate itself
@@ -10,21 +11,63 @@ use serde::Serialize;
 #[derive(Debug, Clone, Serialize)]
 #[serde(tag = "event", content = "args")]
 pub enum Event {
-    ProfilePulledFromStorage { uid: UID },
-    ProfilePushedToStorage { uid: UID },
-    UserPulledFromAPI { uid: UID },
-    UserPushedToAPI { uid: UID },
-    AddonsPulledFromAPI { uid: UID },
-    AddonsPushedToAPI { uid: UID },
-    UserAuthenticated { uid: UID, auth_request: AuthRequest },
-    UserLoggedOut { uid: UID },
-    SessionDeleted { uid: UID },
-    AddonInstalled { uid: UID, addon: Descriptor },
-    AddonUninstalled { uid: UID, addon: Descriptor },
-    SettingsUpdated { uid: UID },
-    LibraryPulledFromStorage { uid: UID },
-    LibraryPushedToStorage { uid: UID },
-    LibrarySyncedWithAPI { uid: UID },
-    LibraryPushedToAPI { uid: UID },
-    Error { error: CtxError, source: Box<Event> },
+    ProfilePulledFromStorage {
+        uid: UID,
+    },
+    ProfilePushedToStorage {
+        uid: UID,
+    },
+    UserPulledFromAPI {
+        uid: UID,
+    },
+    UserPushedToAPI {
+        uid: UID,
+    },
+    AddonsPulledFromAPI {
+        uid: UID,
+    },
+    AddonsPushedToAPI {
+        uid: UID,
+    },
+    UserAuthenticated {
+        uid: UID,
+        auth_request: AuthRequest,
+    },
+    UserLoggedOut {
+        uid: UID,
+    },
+    SessionDeleted {
+        uid: UID,
+    },
+    AddonInstalled {
+        uid: UID,
+        addon: Descriptor,
+    },
+    AddonUninstalled {
+        uid: UID,
+        addon: Descriptor,
+    },
+    SettingsUpdated {
+        uid: UID,
+    },
+    LibraryPulledFromStorage {
+        uid: UID,
+    },
+    LibraryPushedToStorage {
+        uid: UID,
+    },
+    LibrarySyncedWithAPI {
+        uid: UID,
+    },
+    LibraryPushedToAPI {
+        uid: UID,
+    },
+    StreamingServerSettingsUpdated {
+        #[serde(with = "url_serde")]
+        url: Url,
+    },
+    Error {
+        error: CtxError,
+        source: Box<Event>,
+    },
 }
