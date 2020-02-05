5f4041025d3004b947fc555b30fc3d8ade25fda9
nklhrstv <me@nikolahristov.com>
2020-02-05 22:36:42 +0200

    streaming server model removed

--- src/app_model.rs
+++ src/app_model.rs
@@ -9,7 +9,6 @@ use stremio_core::state_types::models::ctx::Ctx;
 use stremio_core::state_types::models::library_filtered::LibraryFiltered;
 use stremio_core::state_types::models::meta_details::MetaDetails;
 use stremio_core::state_types::models::player::Player;
-use stremio_core::state_types::models::streaming_server::StreamingServerLoadable;
 use stremio_core::types::addons::DescriptorPreview;
 use stremio_core::types::MetaPreview;
 use stremio_derive::Model;
@@ -17,16 +16,15 @@ use stremio_derive::Model;
 #[derive(Model, Default, Serialize)]
 pub struct AppModel {
     pub ctx: Ctx<Env>,
+    pub library_items: LibraryItems,
     pub continue_watching: ContinueWatching,
     pub board: CatalogsWithExtra,
     pub discover: CatalogWithFilters<MetaPreview>,
     pub library: LibraryFiltered,
     pub search: CatalogsWithExtra,
     pub meta_details: MetaDetails,
-    pub addon_details: AddonDetails,
     pub addons: CatalogWithFilters<DescriptorPreview>,
-    pub streaming_server: StreamingServerLoadable,
-    pub library_items: LibraryItems,
+    pub addon_details: AddonDetails,
     pub player: Player,
 }
 
@@ -34,15 +32,14 @@ pub struct AppModel {
 #[serde(rename_all = "snake_case")]
 pub enum ModelFieldName {
     Ctx,
+    LibraryItems,
     ContinueWatching,
     Board,
     Discover,
     Library,
     Search,
     MetaDetails,
-    AddonDetails,
     Addons,
-    StreamingServer,
-    LibraryItems,
+    AddonDetails,
     Player,
 }
--- src/stremio_core_web.rs
+++ src/stremio_core_web.rs
@@ -26,13 +26,15 @@ impl StremioCoreWeb {
         })));
         StremioCoreWeb { runtime }
     }
-
     pub fn dispatch(&self, action: &JsValue, model_field: &JsValue) -> JsValue {
         if let Ok(action) = action.into_serde::<Action>() {
             let message = Msg::Action(action);
             let effects = if let Ok(model_field) = model_field.into_serde::<ModelFieldName>() {
                 self.runtime.dispatch_with(|model| match model_field {
                     ModelFieldName::Ctx => model.ctx.update(&message),
+                    ModelFieldName::LibraryItems => {
+                        model.library_items.update(&model.ctx, &message)
+                    }
                     ModelFieldName::ContinueWatching => {
                         model.continue_watching.update(&model.ctx, &message)
                     }
@@ -41,15 +43,9 @@ impl StremioCoreWeb {
                     ModelFieldName::Library => model.library.update(&model.ctx, &message),
                     ModelFieldName::Search => model.search.update(&model.ctx, &message),
                     ModelFieldName::MetaDetails => model.meta_details.update(&model.ctx, &message),
+                    ModelFieldName::Addons => model.addons.update(&model.ctx, &message),
                     ModelFieldName::AddonDetails => {
                         model.addon_details.update(&model.ctx, &message)
-                    }
-                    ModelFieldName::Addons => model.addons.update(&model.ctx, &message),
-                    ModelFieldName::StreamingServer => {
-                        model.streaming_server.update(&model.ctx, &message)
-                    }
-                    ModelFieldName::LibraryItems => {
-                        model.library_items.update(&model.ctx, &message)
                     }
                     ModelFieldName::Player => model.player.update(&model.ctx, &message),
                 })
@@ -62,12 +58,12 @@ impl StremioCoreWeb {
             JsValue::from(false)
         }
     }
-
     pub fn get_state(&self, model_field: &JsValue) -> JsValue {
         let model = &*self.runtime.app.read().unwrap();
         if let Ok(model_field) = model_field.into_serde::<ModelFieldName>() {
             match model_field {
                 ModelFieldName::Ctx => JsValue::from_serde(&model.ctx).unwrap(),
+                ModelFieldName::LibraryItems => JsValue::from_serde(&model.library_items).unwrap(),
                 ModelFieldName::ContinueWatching => {
                     JsValue::from_serde(&model.continue_watching).unwrap()
                 }
@@ -76,12 +72,8 @@ impl StremioCoreWeb {
                 ModelFieldName::Library => JsValue::from_serde(&model.library).unwrap(),
                 ModelFieldName::Search => JsValue::from_serde(&model.search).unwrap(),
                 ModelFieldName::MetaDetails => JsValue::from_serde(&model.meta_details).unwrap(),
-                ModelFieldName::AddonDetails => JsValue::from_serde(&model.addon_details).unwrap(),
                 ModelFieldName::Addons => JsValue::from_serde(&model.addons).unwrap(),
-                ModelFieldName::StreamingServer => {
-                    JsValue::from_serde(&model.streaming_server).unwrap()
-                }
-                ModelFieldName::LibraryItems => JsValue::from_serde(&model.library_items).unwrap(),
+                ModelFieldName::AddonDetails => JsValue::from_serde(&model.addon_details).unwrap(),
                 ModelFieldName::Player => JsValue::from_serde(&model.player).unwrap(),
             }
         } else {
