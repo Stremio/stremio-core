2c0deb2ad861dcb22522eef6c469fe2e68388aac
nklhrstv <me@nikolahristov.com>
2020-02-17 14:57:27 +0200

    LibraryItems adapted to changes in Ctx

--- src/models/library_items.rs
+++ src/models/library_items.rs
@@ -1,9 +1,9 @@
 use itertools::Itertools;
 use serde::Serialize;
-use stremio_core::state_types::models::ctx::library_loadable::LibraryLoadable;
 use stremio_core::state_types::models::ctx::Ctx;
 use stremio_core::state_types::msg::{Internal, Msg};
 use stremio_core::state_types::{Effects, Environment, UpdateWithCtx};
+use stremio_core::types::LibBucket;
 
 #[derive(Default, Debug, Clone, Serialize)]
 pub struct LibraryItems {
@@ -13,24 +13,21 @@ pub struct LibraryItems {
 impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for LibraryItems {
     fn update(&mut self, ctx: &Ctx<Env>, msg: &Msg) -> Effects {
         match msg {
-            Msg::Internal(Internal::LibraryChanged) => ids_update(&mut self.ids, &ctx.library),
+            Msg::Internal(Internal::LibraryChanged(_)) => ids_update(&mut self.ids, ctx.library()),
             _ => Effects::none().unchanged(),
         }
     }
 }
 
-fn ids_update(ids: &mut Vec<String>, library: &LibraryLoadable) -> Effects {
-    let next_ids = match library {
-        LibraryLoadable::Ready(bucket) => bucket
-            .items
-            .values()
-            .filter(|lib_item| !lib_item.removed)
-            .map(|lib_item| &lib_item.id)
-            .cloned()
-            .unique()
-            .collect(),
-        _ => vec![],
-    };
+fn ids_update(ids: &mut Vec<String>, library: &LibBucket) -> Effects {
+    let next_ids = library
+        .items
+        .values()
+        .filter(|lib_item| !lib_item.removed)
+        .map(|lib_item| &lib_item.id)
+        .cloned()
+        .unique()
+        .collect::<Vec<_>>();
     if next_ids.ne(ids) {
         *ids = next_ids;
         Effects::none()
