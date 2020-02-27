5a5e52dabf21f0cbf79b78564d5dfdf4652ac219
Vladimir Borisov <borisov1024@gmail.com>
2020-02-27 17:44:55 +0200

    FIxed a variable name

--- src/app_model.rs
+++ src/app_model.rs
@@ -6,7 +6,7 @@ use stremio_core::state_types::models::catalog_with_filters::CatalogWithFilters;
 use stremio_core::state_types::models::catalogs_with_extra::CatalogsWithExtra;
 use stremio_core::state_types::models::continue_watching::ContinueWatching;
 use stremio_core::state_types::models::ctx::Ctx;
-use stremio_core::state_types::models::library_filtered::LibraryFiltered;
+use stremio_core::state_types::models::library_with_filters::LibraryWithFilters;
 use stremio_core::state_types::models::meta_details::MetaDetails;
 use stremio_core::state_types::models::player::Player;
 use stremio_core::state_types::models::streaming_server::StreamingServer;
@@ -21,7 +21,7 @@ pub struct AppModel {
     pub continue_watching: ContinueWatching,
     pub board: CatalogsWithExtra,
     pub discover: CatalogWithFilters<MetaPreview>,
-    pub library: LibraryFiltered,
+    pub library: LibraryWithFilters,
     pub search: CatalogsWithExtra,
     pub meta_details: MetaDetails,
     pub addons: CatalogWithFilters<DescriptorPreview>,
