9e6ded8605d9ef7ed90642cdf72ed8ab3edf8203
nklhrstv <me@nikolahristov.com>
2020-03-19 18:14:25 +0200

    lib_item added to player model

--- src/state_types/models/player.rs
+++ src/state_types/models/player.rs
@@ -7,7 +7,8 @@ use crate::state_types::msg::{Action, ActionLoad, ActionPlayer, Internal, Msg};
 use crate::state_types::{Effects, Environment, UpdateWithCtx};
 use crate::types::addons::{AggrRequest, ResourceRef, ResourceRequest};
 use crate::types::profile::Settings as ProfileSettings;
-use crate::types::{MetaDetail, Stream, SubtitlesSource, Video};
+use crate::types::{LibBucket, LibItem, LibItemState, MetaDetail, Stream, SubtitlesSource, Video};
+use chrono::Datelike;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -29,6 +30,8 @@ pub struct Player {
     pub meta_resource: Option<ResourceLoadable<MetaDetail>>,
     pub subtitles_resources: Vec<ResourceLoadable<Vec<SubtitlesSource>>>,
     pub next_video: Option<Video>,
+    #[serde(skip)]
+    lib_item: Option<LibItem>,
 }
 
 impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for Player {
@@ -64,10 +67,13 @@ impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for Player {
                         .and_then(|selected| selected.video_id.to_owned()),
                     &ctx.profile().settings,
                 );
+                let lib_item_effects =
+                    lib_item_update::<Env>(&mut self.lib_item, &self.meta_resource, &ctx.library());
                 selected_effects
                     .join(meta_effects)
                     .join(subtitles_effects)
                     .join(next_video_effects)
+                    .join(lib_item_effects)
             }
             Msg::Action(Action::Unload) => {
                 let selected_effects = eq_update(&mut self.selected, None);
@@ -82,10 +88,13 @@ impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for Player {
                         .and_then(|selected| selected.video_id.to_owned()),
                     &ctx.profile().settings,
                 );
+                let lib_item_effects =
+                    lib_item_update::<Env>(&mut self.lib_item, &self.meta_resource, &ctx.library());
                 selected_effects
                     .join(meta_effects)
                     .join(subtitles_effects)
                     .join(next_video_effects)
+                    .join(lib_item_effects)
             }
             Msg::Action(Action::Player(ActionPlayer::TimeChanged { time, duration })) => {
                 match &self.selected {
@@ -137,9 +146,12 @@ impl<Env: Environment + 'static> UpdateWithCtx<Ctx<Env>> for Player {
                         .and_then(|selected| selected.video_id.to_owned()),
                     &ctx.profile().settings,
                 );
+                let lib_item_effects =
+                    lib_item_update::<Env>(&mut self.lib_item, &self.meta_resource, &ctx.library());
                 meta_effects
                     .join(subtitles_effects)
                     .join(next_video_effects)
+                    .join(lib_item_effects)
             }
             _ => Effects::none().unchanged(),
         }
@@ -169,6 +181,56 @@ fn next_video_update(
     };
     if next_video.ne(video) {
         *video = next_video;
+        Effects::none()
+    } else {
+        Effects::none().unchanged()
+    }
+}
+
+fn lib_item_update<Env: Environment>(
+    lib_item: &mut Option<LibItem>,
+    meta_resource: &Option<ResourceLoadable<MetaDetail>>,
+    library: &LibBucket,
+) -> Effects {
+    let next_lib_item = match meta_resource {
+        Some(meta_resource) => match lib_item {
+            Some(LibItem { id, .. }) if id == meta_resource.request.path.id => lib_item.to_owned(),
+            _ => library
+                .items
+                .get(&meta_resource.request.path.id)
+                .cloned()
+                .or_else(|| match meta_resource {
+                    ResourceLoadable {
+                        content: ResourceContent::Ready(meta_detail),
+                        ..
+                    } => Some(LibItem {
+                        id: meta_detail.id.to_owned(),
+                        type_name: meta_detail.type_name.to_owned(),
+                        name: meta_detail.name.to_owned(),
+                        poster: meta_detail.poster.to_owned(),
+                        background: None,
+                        logo: meta_detail.logo.to_owned(),
+                        poster_shape: meta_detail.poster_shape.to_owned(),
+                        year: if let Some(released) = &meta_detail.released {
+                            Some(released.year().to_string())
+                        } else if let Some(release_info) = &meta_detail.release_info {
+                            Some(release_info.to_owned())
+                        } else {
+                            None
+                        },
+                        removed: true,
+                        temp: true,
+                        ctime: Some(Env::now()),
+                        mtime: Env::now(),
+                        state: LibItemState::default(),
+                    }),
+                    _ => None,
+                }),
+        },
+        _ => None,
+    };
+    if next_lib_item.ne(lib_item) {
+        *lib_item = next_lib_item;
         Effects::none()
     } else {
         Effects::none().unchanged()
