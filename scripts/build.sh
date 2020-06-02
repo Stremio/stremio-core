20fed231e68e4108fb152e5fb6bf5c0b58a14e3f
nklhrstv <me@nikolahristov.com>
2020-06-02 15:03:16 +0300

    build and static dir removed from build output

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -ex
 wasm-pack build --no-typescript --out-dir wasm_build --release --target web
-mkdir -p ./static
-mkdir -p ./build
-mv ./wasm_build/stremio_core_web_bg.wasm ./static/stremio_core_web_bg.wasm
-npx babel wasm_build/stremio_core_web.js --config-file ./.babelrc --out-file build/stremio_core_web.js
+mv ./wasm_build/stremio_core_web_bg.wasm stremio_core_web_bg.wasm
+npx babel wasm_build/stremio_core_web.js --config-file ./.babelrc --out-file stremio_core_web.js
