6c762ac6ac353c7ca11db7dd84ea99018cc73752
svetlagasheva <svetulka_24@mail.bg>
2020-07-01 14:33:00 +0300

    signup unit test added

--- src/unit_tests/tests.rs
+++ src/unit_tests/tests.rs
@@ -4,10 +4,12 @@ use crate::state_types::models::ctx::Ctx;
 use crate::state_types::msg::{Action, ActionCtx, Msg};
 use crate::state_types::{EnvFuture, Environment, Runtime};
 use crate::types::api::{
-    APIResult, Auth, AuthRequest, AuthResponse, CollectionResponse, SuccessResponse, True, User,
+    APIResult, Auth, AuthRequest, AuthResponse, CollectionResponse, GDPRConsent, SuccessResponse,
+    True, User,
 };
 use crate::types::profile::{Profile, UID};
 use crate::types::{LibBucket, LibItem};
+use chrono::prelude::*;
 use futures::future;
 use lazy_static::lazy_static;
 use std::any::Any;
@@ -278,5 +280,162 @@ fn actionctx_login() {
 
 #[test]
 fn actionctx_signup() {
-    // TODO
+    #[derive(Model, Debug, Default)]
+    struct Model {
+        ctx: Ctx<Env>,
+    }
+    let (runtime, _) = Runtime::<Env, Model>::new(Model::default(), 1000);
+    assert!(
+        runtime.app.read().unwrap().ctx.profile.auth.is_none(),
+        "there is no user"
+    );
+    lazy_static! {
+        #[derive(Debug)]
+        static ref TEST_USER: User = User {
+            id: "user_id".to_owned(),
+            email: "user_email".to_owned(),
+            fb_id: None,
+            avatar: None,
+            last_modified: Env::now(),
+            date_registered: Env::now(),
+        };
+    }
+    fn fetch_handler(request: Request) -> EnvFuture<Box<dyn Any>> {
+        match request {
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/register"
+                && method == "POST"
+                && body == "{\"type\":\"Auth\",\"type\":\"Register\",\"email\":\"user_email\",\"password\":\"user_password\",\"gdpr_consent\":{\"tos\":true,\"privacy\":true,\"marketing\":false,\"time\":\"2020-01-01T00:00:00Z\",\"from\":\"web\"}}" =>
+            {
+                Box::new(future::ok(Box::new(APIResult::Ok {
+                    result: AuthResponse {
+                        key: "auth_key".to_owned(),
+                        user: TEST_USER.to_owned()
+                    },
+                }) as Box<dyn Any>))
+            }
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/addonCollectionGet"
+                && method == "POST"
+                && body == "{\"type\":\"AddonCollectionGet\",\"authKey\":\"auth_key\",\"update\":true}" =>
+            {
+                Box::new(future::ok(Box::new(APIResult::Ok {
+                    result: CollectionResponse {
+                        addons: vec![],
+                        last_modified: Env::now(),
+                    },
+                }) as Box<dyn Any>))
+            }
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/datastoreGet"
+                && method == "POST"
+                && body == "{\"authKey\":\"auth_key\",\"collection\":\"libraryItem\",\"ids\":[],\"all\":true}" =>
+            {
+                let empty_library: Vec<LibItem> = Vec::new();
+                Box::new(future::ok(Box::new(APIResult::Ok {
+                    result: empty_library,
+                }) as Box<dyn Any>))
+            }
+            _ => default_fetch_handler(request),
+        }
+    }
+    Env::reset();
+    *FETCH_HANDLER.write().unwrap() = Box::new(fetch_handler);
+    let (runtime, _) = Runtime::<Env, Model>::new(Model::default(), 1000);
+    run(
+        runtime.dispatch(&Msg::Action(Action::Ctx(ActionCtx::Authenticate(
+            AuthRequest::Register {
+                email: "user_email".into(),
+                password: "user_password".into(),
+                gdpr_consent: GDPRConsent {
+                    tos: true,
+                    privacy: true,
+                    marketing: false,
+                    time: Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0),
+                    from: "web".to_owned(),
+                },
+            },
+        )))),
+    );
+    assert!(
+        runtime.app.read().unwrap().ctx.profile.auth.is_some(),
+        "profile updated successfully in memory"
+    );
+    assert_eq!(
+        runtime
+            .app
+            .read()
+            .unwrap()
+            .ctx
+            .profile
+            .auth
+            .to_owned()
+            .unwrap()
+            .user,
+        *TEST_USER,
+        "registered user equal to the test user"
+    );
+    assert!(
+        runtime.app.read().unwrap().ctx.library.uid.is_some()
+            && runtime.app.read().unwrap().ctx.library.items.is_empty(),
+        "library updated successfully in memory"
+    );
+    assert!(
+        STORAGE
+            .read()
+            .unwrap()
+            .get(PROFILE_STORAGE_KEY)
+            .map_or(true, |data| {
+                serde_json::from_str::<Profile>(&data)
+                    .unwrap()
+                    .auth
+                    .is_some()
+            }),
+        "profile updated successfully in storage"
+    );
+    // TODO library updated successfully in storage
+    assert_eq!(
+        REQUESTS.read().unwrap().len(),
+        3,
+        "Three requests have been sent"
+    );
+    assert!(
+        match REQUESTS.read().unwrap().get(0).unwrap() {
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/register"
+                && method == "POST"
+                && body == "{\"type\":\"Auth\",\"type\":\"Register\",\"email\":\"user_email\",\"password\":\"user_password\",\"gdpr_consent\":{\"tos\":true,\"privacy\":true,\"marketing\":false,\"time\":\"2020-01-01T00:00:00Z\",\"from\":\"web\"}}" =>
+                true,
+            _ => false,
+        },
+        "Register request has been sent"
+    );
+    assert!(
+        match REQUESTS.read().unwrap().get(1).unwrap() {
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/addonCollectionGet"
+                && method == "POST"
+                && body == "{\"type\":\"AddonCollectionGet\",\"authKey\":\"auth_key\",\"update\":true}" =>
+                true,
+            _ => false,
+        },
+        "AddonCollectionGet request has been sent"
+    );
+    assert!(
+        match REQUESTS.read().unwrap().get(2).unwrap() {
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/datastoreGet"
+                && method == "POST"
+                && body == "{\"authKey\":\"auth_key\",\"collection\":\"libraryItem\",\"ids\":[],\"all\":true}" =>
+                true,
+            _ => false,
+        },
+        "DatastoreGet request has been sent"
+    );
 }
