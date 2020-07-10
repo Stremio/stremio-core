3b8a477e54f06572d70c1924b8a4c1202de3cfa6
svetlagasheva <svetulka_24@mail.bg>
2020-07-10 18:50:02 +0300

    uninstall with user unit test added

--- src/unit_tests/tests.rs
+++ src/unit_tests/tests.rs
@@ -793,6 +793,130 @@ fn actionctx_uninstalladdon_uninstall() {
 }
 
 #[test]
+fn actionctx_uninstalladdon_uninstall_with_user() {
+    #[derive(Model, Debug, Default)]
+    struct Model {
+        ctx: Ctx<Env>,
+    }
+    fn fetch_handler(request: Request) -> EnvFuture<Box<dyn Any>> {
+        match request {
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/addonCollectionSet"
+                && method == "POST"
+                && body == "{\"type\":\"AddonCollectionSet\",\"authKey\":\"auth_key\",\"addons\":[]}" =>
+            {
+                Box::new(future::ok(Box::new(APIResult::Ok {
+                    result: SuccessResponse { success: True {} },
+                }) as Box<dyn Any>))
+            }
+            _ => default_fetch_handler(request),
+        }
+    }
+    let addon = Descriptor {
+        manifest: Manifest {
+            id: "id".to_owned(),
+            version: Version::new(0, 0, 1),
+            name: "name".to_owned(),
+            contact_email: None,
+            description: None,
+            logo: None,
+            background: None,
+            types: vec![],
+            resources: vec![],
+            id_prefixes: None,
+            catalogs: vec![],
+            addon_catalogs: vec![],
+            behavior_hints: Default::default(),
+        },
+        transport_url: "transport_url".to_owned(),
+        flags: Default::default(),
+    };
+    Env::reset();
+    *FETCH_HANDLER.write().unwrap() = Box::new(fetch_handler);
+    STORAGE.write().unwrap().insert(
+        PROFILE_STORAGE_KEY.to_owned(),
+        serde_json::to_string(&Profile {
+            auth: Some(Auth {
+                key: "auth_key".to_owned(),
+                user: User {
+                    id: "user_id".to_owned(),
+                    email: "user_email".to_owned(),
+                    fb_id: None,
+                    avatar: None,
+                    last_modified: Env::now(),
+                    date_registered: Env::now(),
+                },
+            }),
+            addons: vec![addon.to_owned()],
+            ..Default::default()
+        })
+        .unwrap(),
+    );
+    let (runtime, _) = Runtime::<Env, Model>::new(
+        Model {
+            ctx: Ctx {
+                profile: Profile {
+                    auth: Some(Auth {
+                        key: "auth_key".to_owned(),
+                        user: User {
+                            id: "user_id".to_owned(),
+                            email: "user_email".to_owned(),
+                            fb_id: None,
+                            avatar: None,
+                            last_modified: Env::now(),
+                            date_registered: Env::now(),
+                        },
+                    }),
+                    addons: vec![addon.to_owned()],
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        },
+        1000,
+    );
+    run(
+        runtime.dispatch(&Msg::Action(Action::Ctx(ActionCtx::UninstallAddon(
+            addon.transport_url,
+        )))),
+    );
+    assert!(
+        runtime.app.read().unwrap().ctx.profile.addons.is_empty(),
+        "addons updated successfully in memory"
+    );
+    assert!(
+        STORAGE
+            .read()
+            .unwrap()
+            .get(PROFILE_STORAGE_KEY)
+            .map_or(true, |data| {
+                serde_json::from_str::<Profile>(&data)
+                    .unwrap()
+                    .addons
+                    .is_empty()
+            }),
+        "addons updated successfully in storage"
+    );
+    assert_eq!(
+        REQUESTS.read().unwrap().len(),
+        1,
+        "One request has been sent"
+    );
+    assert_eq!(
+        REQUESTS.read().unwrap().get(0).unwrap().to_owned(),
+        Request {
+            url: "https://api.strem.io/api/addonCollectionSet".to_owned(),
+            method: "POST".to_owned(),
+            body: "{\"type\":\"AddonCollectionSet\",\"authKey\":\"auth_key\",\"addons\":[]}"
+                .to_owned(),
+            ..Default::default()
+        },
+        "addonCollectionSet request has been sent"
+    );
+}
+
+#[test]
 fn actionctx_uninstalladdon_uninstall_protected() {
     #[derive(Model, Debug, Default)]
     struct Model {
