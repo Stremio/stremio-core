37cd9d1455c867ee65595c7cbd52ccc7c1733e45
svetlagasheva <svetulka_24@mail.bg>
2020-07-13 16:21:02 +0300

    install with user unit tests added

--- src/unit_tests/tests.rs
+++ src/unit_tests/tests.rs
@@ -548,6 +548,106 @@ fn actionctx_installaddon_install() {
 }
 
 #[test]
+fn actionctx_installaddon_install_with_user() {
+    #[derive(Model, Debug, Default)]
+    struct Model {
+        ctx: Ctx<Env>,
+    }
+    fn fetch_handler(request: Request) -> EnvFuture<Box<dyn Any>> {
+        match request {
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/addonCollectionSet"
+                && method == "POST"
+                && body == "{\"type\":\"AddonCollectionSet\",\"authKey\":\"auth_key\",\"addons\":[]}" =>
+            {
+                Box::new(future::ok(Box::new(APIResult::Ok {
+                    result: SuccessResponse { success: True {} },
+                }) as Box<dyn Any>))
+            }
+            _ => default_fetch_handler(request),
+        }
+    }
+    let addon = Descriptor {
+        manifest: Manifest {
+            id: "id".to_owned(),
+            version: Version::new(0, 0, 1),
+            name: "name".to_owned(),
+            contact_email: None,
+            description: None,
+            logo: None,
+            background: None,
+            types: vec![],
+            resources: vec![],
+            id_prefixes: None,
+            catalogs: vec![],
+            addon_catalogs: vec![],
+            behavior_hints: Default::default(),
+        },
+        transport_url: "transport_url".to_owned(),
+        flags: Default::default(),
+    };
+    Env::reset();
+    *FETCH_HANDLER.write().unwrap() = Box::new(fetch_handler);
+    let (runtime, _) = Runtime::<Env, Model>::new(
+        Model {
+            ctx: Ctx {
+                profile: Profile {
+                    auth: Some(Auth {
+                        key: "auth_key".to_owned(),
+                        user: User {
+                            id: "user_id".to_owned(),
+                            email: "user_email".to_owned(),
+                            fb_id: None,
+                            avatar: None,
+                            last_modified: Env::now(),
+                            date_registered: Env::now(),
+                        },
+                    }),
+                    addons: vec![],
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        },
+        1000,
+    );
+    run(
+        runtime.dispatch(&Msg::Action(Action::Ctx(ActionCtx::InstallAddon(
+            addon.to_owned(),
+        )))),
+    );
+    assert_eq!(
+        runtime.app.read().unwrap().ctx.profile.addons[0],
+        addon,
+        "addon installed successfully"
+    );
+    assert_eq!(
+        serde_json::from_str::<Profile>(&STORAGE.read().unwrap().get(PROFILE_STORAGE_KEY).unwrap())
+            .unwrap()
+            .addons[0],
+        addon,
+        "addon updated successfully in storage"
+    );
+    assert_eq!(
+        REQUESTS.read().unwrap().len(),
+        1,
+        "One request has been sent"
+    );
+    assert_eq!(
+        REQUESTS.read().unwrap().get(0).unwrap().to_owned(),
+        Request {
+            url: "https://api.strem.io/api/addonCollectionSet".to_owned(),
+            method: "POST".to_owned(),
+            body: "{\"type\":\"AddonCollectionSet\",\"authKey\":\"auth_key\",\"addons\":[]}"
+                .to_owned(),
+            ..Default::default()
+        },
+        "addonCollectionSet request has been sent"
+    );
+}
+
+#[test]
 fn actionctx_installaddon_update() {
     #[derive(Model, Debug, Default)]
     struct Model {
@@ -653,6 +753,158 @@ fn actionctx_installaddon_update() {
             .addons[0],
         addon,
         "addon updated successfully in storage"
+    );
+}
+
+#[test]
+fn actionctx_installaddon_update_with_user() {
+    #[derive(Model, Debug, Default)]
+    struct Model {
+        ctx: Ctx<Env>,
+    }
+    fn fetch_handler(request: Request) -> EnvFuture<Box<dyn Any>> {
+        match request {
+            Request {
+                url, method, body, ..
+            } if url == "https://api.strem.io/api/addonCollectionSet"
+                && method == "POST"
+                && body == "{\"type\":\"AddonCollectionSet\",\"authKey\":\"auth_key\",\"addons\":[{\"manifest\":{\"id\":\"id1\",\"version\":\"0.0.1\",\"name\":\"name\",\"contactEmail\":null,\"description\":null,\"logo\":null,\"background\":null,\"types\":[],\"resources\":[],\"idPrefixes\":null,\"catalogs\":[],\"addonCatalogs\":[],\"behaviorHints\":{}},\"transportUrl\":\"transport_url1\",\"flags\":{\"official\":false,\"protected\":false}},{\"manifest\":{\"id\":\"id2\",\"version\":\"0.0.1\",\"name\":\"name\",\"contactEmail\":null,\"description\":null,\"logo\":null,\"background\":null,\"types\":[],\"resources\":[],\"idPrefixes\":null,\"catalogs\":[],\"addonCatalogs\":[],\"behaviorHints\":{}},\"transportUrl\":\"transport_url2\",\"flags\":{\"official\":false,\"protected\":false}}]}" =>
+            {
+                Box::new(future::ok(Box::new(APIResult::Ok {
+                    result: SuccessResponse { success: True {} },
+                }) as Box<dyn Any>))
+            }
+            _ => default_fetch_handler(request),
+        }
+    }
+    let addon = Descriptor {
+        manifest: Manifest {
+            id: "id1.0".to_owned(),
+            version: Version::new(0, 0, 1),
+            name: "name".to_owned(),
+            contact_email: None,
+            description: None,
+            logo: None,
+            background: None,
+            types: vec![],
+            resources: vec![],
+            id_prefixes: None,
+            catalogs: vec![],
+            addon_catalogs: vec![],
+            behavior_hints: Default::default(),
+        },
+        transport_url: "transport_url1".to_owned(),
+        flags: Default::default(),
+    };
+    Env::reset();
+    *FETCH_HANDLER.write().unwrap() = Box::new(fetch_handler);
+    let (runtime, _) = Runtime::<Env, Model>::new(
+        Model {
+            ctx: Ctx {
+                profile: Profile {
+                    auth: Some(Auth {
+                        key: "auth_key".to_owned(),
+                        user: User {
+                            id: "user_id".to_owned(),
+                            email: "user_email".to_owned(),
+                            fb_id: None,
+                            avatar: None,
+                            last_modified: Env::now(),
+                            date_registered: Env::now(),
+                        },
+                    }),
+                    addons: vec![
+                        Descriptor {
+                            manifest: Manifest {
+                                id: "id1".to_owned(),
+                                version: Version::new(0, 0, 1),
+                                name: "name".to_owned(),
+                                contact_email: None,
+                                description: None,
+                                logo: None,
+                                background: None,
+                                types: vec![],
+                                resources: vec![],
+                                id_prefixes: None,
+                                catalogs: vec![],
+                                addon_catalogs: vec![],
+                                behavior_hints: Default::default(),
+                            },
+                            transport_url: "transport_url1".to_owned(),
+                            flags: Default::default(),
+                        },
+                        Descriptor {
+                            manifest: Manifest {
+                                id: "id2".to_owned(),
+                                version: Version::new(0, 0, 1),
+                                name: "name".to_owned(),
+                                contact_email: None,
+                                description: None,
+                                logo: None,
+                                background: None,
+                                types: vec![],
+                                resources: vec![],
+                                id_prefixes: None,
+                                catalogs: vec![],
+                                addon_catalogs: vec![],
+                                behavior_hints: Default::default(),
+                            },
+                            transport_url: "transport_url2".to_owned(),
+                            flags: Default::default(),
+                        },
+                    ],
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        },
+        1000,
+    );
+    run(
+        runtime.dispatch(&Msg::Action(Action::Ctx(ActionCtx::InstallAddon(
+            addon.to_owned(),
+        )))),
+    );
+    assert_eq!(
+        runtime.app.read().unwrap().ctx.profile.addons.len(),
+        2,
+        "There are two addons in memory"
+    );
+    assert_eq!(
+        runtime.app.read().unwrap().ctx.profile.addons[0],
+        addon,
+        "addon updated successfully in memory"
+    );
+    assert_eq!(
+        serde_json::from_str::<Profile>(&STORAGE.read().unwrap().get(PROFILE_STORAGE_KEY).unwrap())
+            .unwrap()
+            .addons
+            .len(),
+        2,
+        "There are two addons in storage"
+    );
+    assert_eq!(
+        serde_json::from_str::<Profile>(&STORAGE.read().unwrap().get(PROFILE_STORAGE_KEY).unwrap())
+            .unwrap()
+            .addons[0],
+        addon,
+        "addon updated successfully in storage"
+    );
+    assert_eq!(
+        REQUESTS.read().unwrap().len(),
+        1,
+        "One request has been sent"
+    );
+    assert_eq!(
+        REQUESTS.read().unwrap().get(0).unwrap().to_owned(),
+        Request {
+            url: "https://api.strem.io/api/addonCollectionSet".to_owned(),
+            method: "POST".to_owned(),
+            body: "{\"type\":\"AddonCollectionSet\",\"authKey\":\"auth_key\",\"addons\":[{\"manifest\":{\"id\":\"id1\",\"version\":\"0.0.1\",\"name\":\"name\",\"contactEmail\":null,\"description\":null,\"logo\":null,\"background\":null,\"types\":[],\"resources\":[],\"idPrefixes\":null,\"catalogs\":[],\"addonCatalogs\":[],\"behaviorHints\":{}},\"transportUrl\":\"transport_url1\",\"flags\":{\"official\":false,\"protected\":false}},{\"manifest\":{\"id\":\"id2\",\"version\":\"0.0.1\",\"name\":\"name\",\"contactEmail\":null,\"description\":null,\"logo\":null,\"background\":null,\"types\":[],\"resources\":[],\"idPrefixes\":null,\"catalogs\":[],\"addonCatalogs\":[],\"behaviorHints\":{}},\"transportUrl\":\"transport_url2\",\"flags\":{\"official\":false,\"protected\":false}}]}"
+                .to_owned(),
+            ..Default::default()
+        },
+        "addonCollectionSet request has been sent"
     );
 }
 
