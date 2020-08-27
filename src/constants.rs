628597dd862180004ac6a253936eb7b1c50841dc
svetlagasheva <svetulka_24@mail.bg>
2020-08-27 16:07:59 +0300

    DEFAULT_ADDON added in constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,6 @@
-use crate::types::addons::Descriptor;
+use crate::types::addons::{Descriptor, Manifest};
 use lazy_static::lazy_static;
+use semver::Version;
 
 pub const API_URL: &str = "https://api.strem.io";
 pub const STREAMING_SERVER_URL: &str = "http://127.0.0.1:11470";
@@ -21,4 +22,23 @@ lazy_static! {
     pub static ref OFFICIAL_ADDONS: Vec<Descriptor> =
         serde_json::from_slice(include_bytes!("../stremio-official-addons/index.json"))
             .expect("official addons JSON parse");
+    pub static ref DEFAULT_ADDON: Descriptor = Descriptor {
+        manifest: Manifest {
+            id: "id".to_owned(),
+            version: Version::new(0, 0, 1),
+            name: "name".to_owned(),
+            contact_email: None,
+            description: None,
+            logo: None,
+            background: None,
+            types: vec![],
+            resources: vec![],
+            id_prefixes: None,
+            catalogs: vec![],
+            addon_catalogs: vec![],
+            behavior_hints: Default::default(),
+        },
+        transport_url: "transport_url".to_owned(),
+        flags: Default::default(),
+    };
 }
