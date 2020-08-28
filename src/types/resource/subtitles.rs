d2a1a009da6e858f87733c867cd8f2838823e599
nklhrstv <me@nikolahristov.com>
2020-08-28 15:47:17 +0300

    use Url type for subtitles url

--- src/types/resource/subtitles.rs
+++ src/types/resource/subtitles.rs
@@ -1,9 +1,10 @@
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Subtitles {
     pub id: String,
     // @TODO: ISO 639-2
     pub lang: String,
-    pub url: String,
+    pub url: Url,
 }
