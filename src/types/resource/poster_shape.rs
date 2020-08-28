b160c5e91bced1192472445ad1fe15e8d8d2a9ea
nklhrstv <me@nikolahristov.com>
2020-08-28 14:32:59 +0300

    remove Eq and Ord from PosterShape

--- src/types/resource/poster_shape.rs
+++ src/types/resource/poster_shape.rs
@@ -1,7 +1,7 @@
 use derivative::Derivative;
 use serde::{Deserialize, Serialize};
 
-#[derive(Derivative, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+#[derive(Derivative, Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[derivative(Default)]
 #[serde(rename_all = "camelCase")]
 pub enum PosterShape {
