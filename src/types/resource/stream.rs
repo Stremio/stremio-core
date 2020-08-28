8f64b6ff6ab551b49f27f00659d6b64be9d33ee0
nklhrstv <me@nikolahristov.com>
2020-08-28 14:33:28 +0300

    remove Eq from StreamSouce

--- src/types/resource/stream.rs
+++ src/types/resource/stream.rs
@@ -40,7 +40,7 @@ impl Stream {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum StreamSource {
     Url {
