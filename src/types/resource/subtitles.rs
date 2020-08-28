c477deb2968cfcaa6f215e2f96690630f66f13c8
nklhrstv <me@nikolahristov.com>
2020-08-28 14:33:13 +0300

    remove Eq from subtitles

--- src/types/resource/subtitles.rs
+++ src/types/resource/subtitles.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Subtitles {
     pub id: String,
     // @TODO: ISO 639-2
