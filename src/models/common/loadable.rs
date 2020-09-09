34d16366340d2ea046c3a4dbd9b95f2dd022fba8
nklhrstv <me@nikolahristov.com>
2020-09-09 13:07:05 +0300

    use Derivative for implementing Default on Loadable

--- src/models/common/loadable.rs
+++ src/models/common/loadable.rs
@@ -1,15 +1,12 @@
+use derivative::Derivative;
 use serde::Serialize;
 
-#[derive(Clone, PartialEq, Serialize)]
+#[derive(Derivative, Clone, PartialEq, Serialize)]
+#[derivative(Default)]
 #[serde(tag = "type", content = "content")]
 pub enum Loadable<R, E> {
+    #[derivative(Default)]
     Loading,
     Ready(R),
     Err(E),
-}
-
-impl<R, E> Default for Loadable<R, E> {
-    fn default() -> Self {
-        Loadable::Loading
-    }
 }
