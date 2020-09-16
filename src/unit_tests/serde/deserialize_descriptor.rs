364428be2bae644b603ee1e126640ce4591b9130
svetlagasheva <svetulka_24@mail.bg>
2020-09-16 11:54:07 +0300

    descriptor deserializing test (serde(default) are omited) added

--- src/unit_tests/serde/deserialize_descriptor.rs
+++ src/unit_tests/serde/deserialize_descriptor.rs
@@ -7,135 +7,173 @@ use url::Url;
 
 #[test]
 fn deserialize_descriptor() {
-    let descriptor = Descriptor {
-        manifest: Manifest {
-            id: "id".to_owned(),
-            version: Version::new(0, 0, 1),
-            name: "name".to_owned(),
-            contact_email: Some("contact_email".to_owned()),
-            description: Some("description".to_owned()),
-            logo: Some("logo".to_owned()),
-            background: Some("background".to_owned()),
-            types: vec!["type".to_owned()],
-            resources: vec![
-                ManifestResource::Short("resource".to_owned()),
-                ManifestResource::Full {
-                    name: "name".to_owned(),
-                    types: Some(vec!["type".to_owned()]),
-                    id_prefixes: Some(vec!["id_prefix".to_owned()]),
-                },
-            ],
-            id_prefixes: Some(vec!["id_prefix".to_owned()]),
-            catalogs: vec![ManifestCatalog {
-                type_name: "type_name".to_owned(),
+    let descriptors = vec![
+        Descriptor {
+            manifest: Manifest {
                 id: "id".to_owned(),
-                name: Some("name".to_owned()),
-                extra: ManifestExtra::Full {
-                    props: vec![ManifestExtraProp {
+                version: Version::new(0, 0, 1),
+                name: "name".to_owned(),
+                contact_email: Some("contact_email".to_owned()),
+                description: Some("description".to_owned()),
+                logo: Some("logo".to_owned()),
+                background: Some("background".to_owned()),
+                types: vec!["type".to_owned()],
+                resources: vec![
+                    ManifestResource::Short("resource".to_owned()),
+                    ManifestResource::Full {
                         name: "name".to_owned(),
-                        is_required: true,
-                        options: Some(vec!["option".to_owned()]),
-                        options_limit: OptionsLimit(1),
-                    }],
-                },
-            }],
-            addon_catalogs: vec![ManifestCatalog {
-                type_name: "type_name".to_owned(),
+                        types: Some(vec!["type".to_owned()]),
+                        id_prefixes: Some(vec!["id_prefix".to_owned()]),
+                    },
+                ],
+                id_prefixes: Some(vec!["id_prefix".to_owned()]),
+                catalogs: vec![ManifestCatalog {
+                    type_name: "type_name".to_owned(),
+                    id: "id".to_owned(),
+                    name: Some("name".to_owned()),
+                    extra: ManifestExtra::Full {
+                        props: vec![ManifestExtraProp {
+                            name: "name".to_owned(),
+                            is_required: true,
+                            options: Some(vec!["option".to_owned()]),
+                            options_limit: OptionsLimit(1),
+                        }],
+                    },
+                }],
+                addon_catalogs: vec![ManifestCatalog {
+                    type_name: "type_name".to_owned(),
+                    id: "id".to_owned(),
+                    name: Some("name".to_owned()),
+                    extra: ManifestExtra::Short {
+                        required: vec!["required".to_owned()],
+                        supported: vec!["supported".to_owned()],
+                    },
+                }],
+                behavior_hints: vec![("behavior_hint".to_owned(), serde_json::Value::Bool(true))]
+                    .iter()
+                    .cloned()
+                    .collect(),
+            },
+            transport_url: Url::parse("https://transport_url").unwrap(),
+            flags: DescriptorFlags {
+                official: true,
+                protected: true,
+                extra: vec![("flag".to_owned(), serde_json::Value::Bool(true))]
+                    .iter()
+                    .cloned()
+                    .collect(),
+            },
+        },
+        Descriptor {
+            manifest: Manifest {
                 id: "id".to_owned(),
-                name: Some("name".to_owned()),
-                extra: ManifestExtra::Short {
-                    required: vec!["required".to_owned()],
-                    supported: vec!["supported".to_owned()],
-                },
-            }],
-            behavior_hints: vec![("behavior_hint".to_owned(), serde_json::Value::Bool(true))]
-                .iter()
-                .cloned()
-                .collect(),
+                version: Version::new(0, 0, 1),
+                name: "name".to_owned(),
+                contact_email: None,
+                description: None,
+                logo: None,
+                background: None,
+                types: vec![],
+                resources: vec![],
+                id_prefixes: None,
+                catalogs: vec![],
+                addon_catalogs: vec![],
+                behavior_hints: vec![].iter().cloned().collect(),
+            },
+            transport_url: Url::parse("https://transport_url").unwrap(),
+            flags: DescriptorFlags {
+                official: false,
+                protected: false,
+                extra: vec![].iter().cloned().collect(),
+            },
         },
-        transport_url: Url::parse("https://transport_url").unwrap(),
-        flags: DescriptorFlags {
-            official: true,
-            protected: true,
-            extra: vec![("flag".to_owned(), serde_json::Value::Bool(true))]
-                .iter()
-                .cloned()
-                .collect(),
-        },
-    };
-    let descriptor_json = r#"
-    {
-        "manifest": {
-            "id": "id",
-            "version": "0.0.1",
-            "name": "name",
-            "contactEmail": "contact_email",
-            "description": "description",
-            "logo": "logo",
-            "background": "background",
-            "types": [
-                "type"
-            ],
-            "resources": [
-                "resource",
-                {
-                    "name": "name",
-                    "types": [
-                        "type"
-                    ],
-                    "idPrefixes": [
-                        "id_prefix"
-                    ]
+    ];
+    let descriptors_json = r#"
+    [
+        {
+            "manifest": {
+                "id": "id",
+                "version": "0.0.1",
+                "name": "name",
+                "contactEmail": "contact_email",
+                "description": "description",
+                "logo": "logo",
+                "background": "background",
+                "types": [
+                    "type"
+                ],
+                "resources": [
+                    "resource",
+                    {
+                        "name": "name",
+                        "types": [
+                            "type"
+                        ],
+                        "idPrefixes": [
+                            "id_prefix"
+                        ]
+                    }
+                ],
+                "idPrefixes": [
+                    "id_prefix"
+                ],
+                "catalogs": [
+                    {
+                        "type": "type_name",
+                        "id": "id",
+                        "name": "name",
+                        "extra": [
+                            {
+                                "name": "name",
+                                "isRequired": true,
+                                "options": [
+                                    "option"
+                                ],
+                                "optionsLimit": 1
+                            }
+                        ]
+                    }
+                ],
+                "addonCatalogs": [
+                    {
+                        "type": "type_name",
+                        "id": "id",
+                        "name": "name",
+                        "extraRequired": [
+                            "required"
+                        ],
+                        "extraSupported": [
+                            "supported"
+                        ]
+                    }
+                ],
+                "behaviorHints": {
+                    "behavior_hint": true
                 }
-            ],
-            "idPrefixes": [
-                "id_prefix"
-            ],
-            "catalogs": [
-                {
-                    "type": "type_name",
-                    "id": "id",
-                    "name": "name",
-                    "extra": [
-                        {
-                            "name": "name",
-                            "isRequired": true,
-                            "options": [
-                                "option"
-                            ],
-                            "optionsLimit": 1
-                        }
-                    ]
-                }
-            ],
-            "addonCatalogs": [
-                {
-                    "type": "type_name",
-                    "id": "id",
-                    "name": "name",
-                    "extraRequired": [
-                        "required"
-                    ],
-                    "extraSupported": [
-                        "supported"
-                    ]
-                }
-            ],
-            "behaviorHints": {
-                "behavior_hint": true
+            },
+            "transportUrl": "https://transport_url/",
+            "flags": {
+                "official": true,
+                "protected": true,
+                "flag": true
             }
         },
-        "transportUrl": "https://transport_url/",
-        "flags": {
-            "official": true,
-            "protected": true,
-            "flag": true
+        {
+            "manifest": {
+                "id": "id",
+                "version": "0.0.1",
+                "name": "name",
+                "types": [],
+                "resources": [],
+                "idPrefixes": null
+            },
+            "transportUrl": "https://transport_url/"
         }
-    }
+    ]
     "#;
-    let descriptor_deserialize: Descriptor = serde_json::from_str(&descriptor_json).unwrap();
+    let descriptors_deserialize: Vec<Descriptor> = serde_json::from_str(&descriptors_json).unwrap();
     assert_eq!(
-        descriptor, descriptor_deserialize,
+        descriptors, descriptors_deserialize,
         "descriptor deserialized successfully"
     );
 }
