69ea48c00cbab2cf8c800b64412885d76463ee32
nklhrstv <me@nikolahristov.com>
2020-09-16 12:52:32 +0300

    replace stremio-official-addons submodule with a rust crate

--- src/constants.rs
+++ src/constants.rs
@@ -25,6 +25,6 @@ lazy_static! {
     pub static ref STREAMING_SERVER_URL: Url =
         Url::parse("http://127.0.0.1:11470").expect("STREAMING_SERVER_URL parse failed");
     pub static ref OFFICIAL_ADDONS: Vec<Descriptor> =
-        serde_json::from_slice(include_bytes!("../stremio-official-addons/addons.json"))
+        serde_json::from_slice(stremio_official_addons::ADDONS)
             .expect("OFFICIAL_ADDONS parse failed");
 }
