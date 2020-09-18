6bcc63c1a1ad8328df23013a9dcacdf80c0fdc16
svetlagasheva <svetulka_24@mail.bg>
2020-09-18 17:42:29 +0300

    format code

--- src/unit_tests/serde/serialize_api_request.rs
+++ src/unit_tests/serde/serialize_api_request.rs
@@ -34,7 +34,8 @@ fn serialize_api_request_addon_collection_set() {
         auth_key: "auth_key".to_owned(),
         addons: vec![],
     };
-    let addon_collection_set_json = r#"{"type":"AddonCollectionSet","authKey":"auth_key","addons":[]}"#;
+    let addon_collection_set_json =
+        r#"{"type":"AddonCollectionSet","authKey":"auth_key","addons":[]}"#;
     let addon_collection_set_serialize = serde_json::to_string(&addon_collection_set).unwrap();
     assert_eq!(
         addon_collection_set_json, addon_collection_set_serialize,
