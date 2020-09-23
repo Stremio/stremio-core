31482d51014d465f0a287ba5e10bbf2972565465
svetlagasheva <svetulka_24@mail.bg>
2020-09-23 15:34:10 +0300

    api result Ok serializing test added

--- src/unit_tests/serde/serialize_api_result.rs
+++ src/unit_tests/serde/serialize_api_result.rs
@@ -1,4 +1,4 @@
-use crate::types::api::{APIError, APIResult};
+use crate::types::api::{APIError, APIResult, SuccessResponse, True};
 
 #[test]
 fn serialize_api_result_err() {
@@ -11,4 +11,14 @@ fn serialize_api_result_err() {
     let error_json = r#"{"error":{"message":"message","code":1}}"#;
     let error_serialize = serde_json::to_string(&error).unwrap();
     assert_eq!(error_json, error_serialize, "Err serialized successfully");
+}
+
+#[test]
+fn serialize_api_result_ok() {
+    let ok = APIResult::<SuccessResponse>::Ok {
+        result: SuccessResponse { success: True {} },
+    };
+    let ok_json = r#"{"result":{"success":true}}"#;
+    let ok_serialize = serde_json::to_string(&ok).unwrap();
+    assert_eq!(ok_json, ok_serialize, "Ok serialized successfully");
 }
