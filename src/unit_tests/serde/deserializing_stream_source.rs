b6555a0c088f4c75ceff98b8ffeff3bf8c1b07d8
svetlagasheva <svetulka_24@mail.bg>
2020-09-24 18:13:20 +0300

    stream source YouTube deserializing test added

--- src/unit_tests/serde/deserializing_stream_source.rs
+++ src/unit_tests/serde/deserializing_stream_source.rs
@@ -12,8 +12,22 @@ fn deserialize_stream_source_url() {
     }
     "#;
     let url_deserialize = serde_json::from_str(&url_json).unwrap();
+    assert_eq!(url, url_deserialize, "Url deserialized successfully");
+}
+
+#[test]
+fn deserialize_stream_source_youtube() {
+    let youtube = StreamSource::YouTube {
+        yt_id: "yt_id".to_owned(),
+    };
+    let youtube_json = r#"
+    {
+        "ytId": "yt_id"
+    }
+    "#;
+    let youtube_deserialize = serde_json::from_str(&youtube_json).unwrap();
     assert_eq!(
-        url, url_deserialize,
-        "Url deserialized successfully"
+        youtube, youtube_deserialize,
+        "YouTube deserialized successfully"
     );
 }
