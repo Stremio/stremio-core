ce999de66332ad698fb82e3e874377a948bfe2a1
svetlagasheva <svetulka_24@mail.bg>
2020-09-24 13:40:35 +0300

    resource response meta deserializing test updated

--- src/unit_tests/serde/deserialize_resource_response.rs
+++ src/unit_tests/serde/deserialize_resource_response.rs
@@ -222,44 +222,163 @@ fn deserialize_resource_response_metas_detailed() {
 
 #[test]
 fn deserialize_resource_response_meta() {
-    let meta_item = MetaItem {
-        id: "id".to_owned(),
-        type_name: "type_name".to_owned(),
-        name: "name".to_owned(),
-        poster: None,
-        background: None,
-        logo: None,
-        popularity: None,
-        description: None,
-        release_info: None,
-        runtime: None,
-        released: None,
-        poster_shape: Default::default(),
-        videos: vec![Video {
-            id: "id".to_owned(),
-            title: "title".to_owned(),
-            released: Some(Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0)),
-            overview: None,
-            thumbnail: None,
-            streams: vec![],
-            series_info: Some(SeriesInfo {
-                season: 1,
-                episode: 1,
-            }),
-            trailer_streams: vec![],
-        }],
-        links: vec![Link {
+    let meta_items = vec![
+        // ALL fields are defined with SOME value
+        MetaItem {
+            id: "id1".to_owned(),
+            type_name: "type_name".to_owned(),
             name: "name".to_owned(),
-            category: "category".to_owned(),
-            url: Url::parse("https://url").unwrap(),
-        }],
-        trailer_streams: vec![],
-        behavior_hints: Default::default(),
-    };
-    let meta_json = r#"
-    {
-        "meta": {
-            "id": "id",
+            poster: Some("poster".to_owned()),
+            background: Some("background".to_owned()),
+            logo: Some("logo".to_owned()),
+            popularity: Some(1.0),
+            description: Some("description".to_owned()),
+            release_info: Some("release_info".to_owned()),
+            runtime: Some("runtime".to_owned()),
+            released: Some(Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0)),
+            poster_shape: PosterShape::Square,
+            videos: vec![Video {
+                id: "id".to_owned(),
+                title: "title".to_owned(),
+                released: Some(Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0)),
+                overview: Some("overview".to_owned()),
+                thumbnail: Some("thumbnail".to_owned()),
+                streams: vec![],
+                series_info: Some(SeriesInfo {
+                    season: 1,
+                    episode: 1,
+                }),
+                trailer_streams: vec![],
+            }],
+            links: vec![Link {
+                name: "name".to_owned(),
+                category: "category".to_owned(),
+                url: Url::parse("https://url").unwrap(),
+            }],
+            trailer_streams: vec![],
+            behavior_hints: MetaItemBehaviorHints {
+                default_video_id: Some("default_video_id".to_owned()),
+                featured_video_id: Some("featured_video_id".to_owned()),
+                has_scheduled_videos: true,
+            },
+        },
+        // serde(default) are omited
+        MetaItem {
+            id: "id2".to_owned(),
+            type_name: "type_name".to_owned(),
+            name: "".to_owned(),
+            poster: None,
+            background: None,
+            logo: None,
+            popularity: None,
+            description: None,
+            release_info: None,
+            runtime: None,
+            released: None,
+            poster_shape: PosterShape::Poster,
+            videos: vec![],
+            links: vec![],
+            trailer_streams: vec![],
+            behavior_hints: MetaItemBehaviorHints {
+                default_video_id: None,
+                featured_video_id: None,
+                has_scheduled_videos: false,
+            },
+        },
+        // ALL NONEs are set to null.
+        // poster shape is invalid
+        // title, streams, trailer_streams, has_scheduled_videos are omited
+        MetaItem {
+            id: "id3".to_owned(),
+            type_name: "type_name".to_owned(),
+            name: "name".to_owned(),
+            poster: None,
+            background: None,
+            logo: None,
+            popularity: None,
+            description: None,
+            release_info: None,
+            runtime: None,
+            released: None,
+            poster_shape: PosterShape::Poster,
+            videos: vec![Video {
+                id: "id".to_owned(),
+                title: "".to_owned(),
+                released: None,
+                overview: None,
+                thumbnail: None,
+                streams: vec![],
+                series_info: Some(SeriesInfo {
+                    season: 1,
+                    episode: 1,
+                }),
+                trailer_streams: vec![],
+            }],
+            links: vec![],
+            trailer_streams: vec![],
+            behavior_hints: MetaItemBehaviorHints {
+                default_video_id: None,
+                featured_video_id: None,
+                has_scheduled_videos: false,
+            },
+        },
+    ];
+    let meta_items_json = r#"
+    [
+        {
+            "id": "id1",
+            "type": "type_name",
+            "name": "name",
+            "poster": "poster",
+            "background": "background",
+            "logo": "logo",
+            "popularity": 1.0,
+            "description": "description",
+            "releaseInfo": "release_info",
+            "runtime": "runtime",
+            "released": "2020-01-01T00:00:00Z",
+            "posterShape": "square",
+            "videos": [
+                {
+                    "id": "id",
+                    "title": "title",
+                    "released": "2020-01-01T00:00:00Z",
+                    "overview": "overview",
+                    "thumbnail": "thumbnail",
+                    "streams": [],
+                    "season": 1,
+                    "episode": 1,
+                    "trailerStreams": []
+                }
+            ],
+            "links": [
+                {
+                    "name": "name",
+                    "category": "category",
+                    "url": "https://url/"
+                }
+            ],
+            "trailerStreams": [],
+            "behaviorHints": {
+                "defaultVideoId": "default_video_id",
+                "featuredVideoId": "featured_video_id",
+                "hasScheduledVideos": true
+            }
+        },
+        {
+            "id": "id2",
+            "type": "type_name",
+            "poster": null,
+            "background": null,
+            "logo": null,
+            "popularity": null,
+            "description": null,
+            "releaseInfo": null,
+            "runtime": null,
+            "released": null
+        },
+        {
+            "id": "id3",
             "type": "type_name",
             "name": "name",
             "poster": null,
@@ -270,40 +389,31 @@ fn deserialize_resource_response_meta() {
             "releaseInfo": null,
             "runtime": null,
             "released": null,
-            "posterShape": "poster",
+            "posterShape": "invalid",
             "videos": [
                 {
                     "id": "id",
-                    "title": "title",
-                    "released": "2020-01-01T00:00:00Z",
+                    "released": null,
                     "overview": null,
                     "thumbnail": null,
-                    "streams": [],
                     "season": 1,
-                    "episode": 1,
-                    "trailers": []
+                    "episode": 1
                 }
             ],
-            "links": [
-                {
-                    "name": "name",
-                    "category": "category",
-                    "url": "https://url/"
-                }
-            ],
-            "trailers": [],
+            "links": [],
+            "trailerStreams": [],
             "behaviorHints": {
                 "defaultVideoId": null,
                 "featuredVideoId": null
             }
         }
-    }
+    ]
     "#;
-    let meta_deserialize = serde_json::from_str(&meta_json).unwrap();
-    match meta_deserialize {
-        Meta { meta } => assert_eq!(meta, meta_item, "meta deserialized successfully"),
-        _ => panic!("failed getting meta"),
-    };
+    let meta_items_deserialize: Vec<MetaItem> = serde_json::from_str(&meta_items_json).unwrap();
+    assert_eq!(
+        meta_items, meta_items_deserialize,
+        "meta items deserialized successfully"
+    );
 }
 
 #[test]
