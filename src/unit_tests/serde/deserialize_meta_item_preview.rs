63a62442b5528d7875615796b6ec4fa857685e96
svetlagasheva <svetulka_24@mail.bg>
2020-10-01 14:58:34 +0300

    use write macro in meta item preview deserializing test

--- src/unit_tests/serde/deserialize_meta_item_preview.rs
+++ src/unit_tests/serde/deserialize_meta_item_preview.rs
@@ -1,6 +1,7 @@
 use crate::types::resource::{MetaItemBehaviorHints, MetaItemPreview, PosterShape};
 use chrono::prelude::TimeZone;
 use chrono::Utc;
+use std::fmt::Write;
 
 #[test]
 fn deserialize_meta_item_preview() {
@@ -52,56 +53,55 @@ fn deserialize_meta_item_preview() {
             behavior_hints: MetaItemBehaviorHints::default(),
         },
     ];
-    let meta_item_previews_json = r#"
-    [
-        {
-            "id": "id1",
-            "type": "type_name",
-            "name": "name",
-            "poster": "poster",
-            "logo": "logo",
-            "description": "description",
-            "releaseInfo": "release_info",
-            "runtime": "runtime",
-            "released": "2020-01-01T00:00:00Z",
-            "posterShape": "square",
-            "trailerStreams": [],
-            "behaviorHints": {
-                "defaultVideoId": null,
-                "featuredVideoId": null,
-                "hasScheduledVideos": false
-            }
-        },
-        {
-            "id": "id2",
-            "type": "type_name",
-            "poster": null,
-            "logo": null,
-            "description": null,
-            "releaseInfo": null,
-            "runtime": null,
-            "released": null
-        },
-        {
-            "id": "id3",
-            "type": "type_name",
-            "name": "name",
-            "poster": null,
-            "logo": null,
-            "description": null,
-            "releaseInfo": null,
-            "runtime": null,
-            "released": null,
-            "posterShape": "invalid",
-            "trailerStreams": [],
-            "behaviorHints": {
-                "defaultVideoId": null,
-                "featuredVideoId": null,
-                "hasScheduledVideos": false
-            }
-        }
-    ]
-    "#;
+    let mut meta_item_previews_json = "".to_string();
+    write!(
+        meta_item_previews_json,
+        r#"
+        [
+            {{
+                "id": "id1",
+                "type": "type_name",
+                "name": "name",
+                "poster": "poster",
+                "logo": "logo",
+                "description": "description",
+                "releaseInfo": "release_info",
+                "runtime": "runtime",
+                "released": "2020-01-01T00:00:00Z",
+                "posterShape": "square",
+                "trailerStreams": [],
+                "behaviorHints": {}
+            }},
+            {{
+                "id": "id2",
+                "type": "type_name",
+                "poster": null,
+                "logo": null,
+                "description": null,
+                "releaseInfo": null,
+                "runtime": null,
+                "released": null
+            }},
+            {{
+                "id": "id3",
+                "type": "type_name",
+                "name": "name",
+                "poster": null,
+                "logo": null,
+                "description": null,
+                "releaseInfo": null,
+                "runtime": null,
+                "released": null,
+                "posterShape": "invalid",
+                "trailerStreams": [],
+                "behaviorHints": {}
+            }}
+        ]
+        "#,
+        serde_json::to_string(&MetaItemBehaviorHints::default()).unwrap(),
+        serde_json::to_string(&MetaItemBehaviorHints::default()).unwrap()
+    )
+    .unwrap();
     let meta_item_previews_deserialize: Vec<MetaItemPreview> =
         serde_json::from_str(&meta_item_previews_json).unwrap();
     assert_eq!(
