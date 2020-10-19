bde7f20e8b874d692232b71d1af679bfb5a884ae
nklhrstv <me@nikolahristov.com>
2020-10-19 21:22:12 +0300

    fix sort query parameter

--- src/model/deep_links.rs
+++ src/model/deep_links.rs
@@ -249,12 +249,24 @@ impl From<(&String, &LibraryRequest)> for LibraryDeepLinks {
                     "#/{}/{}?{}",
                     root,
                     utf8_percent_encode(&r#type, URI_COMPONENT_ENCODE_SET),
-                    query_params_encode(&[("sort", serde_json::to_string(&request.sort).unwrap())])
+                    query_params_encode(&[(
+                        "sort",
+                        serde_json::to_value(&request.sort)
+                            .unwrap()
+                            .as_str()
+                            .unwrap()
+                    )])
                 ),
                 _ => format!(
                     "#/{}?{}",
                     root,
-                    query_params_encode(&[("sort", serde_json::to_string(&request.sort).unwrap())])
+                    query_params_encode(&[(
+                        "sort",
+                        serde_json::to_value(&request.sort)
+                            .unwrap()
+                            .as_str()
+                            .unwrap()
+                    )])
                 ),
             },
         }
