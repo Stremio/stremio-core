9e300200b390e6cb513fa4c7fa47417b598aa8d4
nklhrstv <me@nikolahristov.com>
2020-10-30 08:57:58 +0200

    LibraryItem in ContinueWatchingPreview schema changed

--- src/model/serialize_continue_watching_preview.rs
+++ src/model/serialize_continue_watching_preview.rs
@@ -1,6 +1,7 @@
 use crate::model::deep_links::{LibraryDeepLinks, LibraryItemDeepLinks};
 use serde::Serialize;
 use stremio_core::models::continue_watching_preview::ContinueWatchingPreview;
+use stremio_core::types::resource::PosterShape;
 use wasm_bindgen::JsValue;
 
 mod model {
@@ -8,8 +9,13 @@ mod model {
     #[derive(Serialize)]
     #[serde(rename_all = "camelCase")]
     pub struct LibraryItem<'a> {
-        #[serde(flatten)]
-        pub library_item: &'a stremio_core::types::library::LibraryItem,
+        #[serde(rename = "_id")]
+        pub id: &'a String,
+        pub name: &'a String,
+        pub r#type: &'a String,
+        pub poster: &'a Option<String>,
+        pub poster_shape: &'a PosterShape,
+        pub progress: f64,
         pub deep_links: LibraryItemDeepLinks,
     }
     #[derive(Serialize)]
@@ -28,7 +34,20 @@ pub fn serialize_continue_watching_preview(
             .library_items
             .iter()
             .map(|library_item| model::LibraryItem {
-                library_item,
+                id: &library_item.id,
+                name: &library_item.name,
+                r#type: &library_item.r#type,
+                poster: &library_item.poster,
+                poster_shape: if library_item.poster_shape == PosterShape::Landscape {
+                    &PosterShape::Square
+                } else {
+                    &library_item.poster_shape
+                },
+                progress: if library_item.state.time_offset > 0 && library_item.state.duration > 0 {
+                    library_item.state.time_offset as f64 / library_item.state.duration as f64
+                } else {
+                    0.0
+                },
                 deep_links: LibraryItemDeepLinks::from(library_item),
             })
             .collect::<Vec<_>>(),
