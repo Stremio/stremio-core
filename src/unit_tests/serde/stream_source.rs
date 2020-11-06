fb92e646a09150dc45b9b261f9548db93c9163cc
svetlagasheva <svetulka_24@mail.bg>
2020-11-06 17:25:21 +0200

    StreamSource deserializing test updated

--- src/unit_tests/serde/stream_source.rs
+++ src/unit_tests/serde/stream_source.rs
@@ -1,26 +1,24 @@
 use crate::types::resource::StreamSource;
-use serde_test::{assert_de_tokens, assert_ser_tokens, assert_tokens, Token};
+use serde_test::{assert_de_tokens, assert_ser_tokens, Token};
 use url::Url;
 
 #[test]
 fn stream_source() {
-    let torrents = vec![
-        StreamSource::Torrent {
-            info_hash: [1; 20],
-            file_idx: Some(1),
-        },
-        StreamSource::Torrent {
-            info_hash: [1; 20],
-            file_idx: None,
-        },
-    ];
-    assert_tokens(
+    assert_ser_tokens(
         &vec![
             StreamSource::Url {
                 url: Url::parse("https://url").unwrap(),
             },
             StreamSource::YouTube {
                 yt_id: "yt_id".to_owned(),
+            },
+            StreamSource::Torrent {
+                info_hash: [1; 20],
+                file_idx: Some(1),
+            },
+            StreamSource::Torrent {
+                info_hash: [1; 20],
+                file_idx: None,
             },
             StreamSource::External {
                 external_url: Url::parse("https://external_url").unwrap(),
@@ -30,7 +28,7 @@ fn stream_source() {
             },
         ],
         &[
-            Token::Seq { len: Some(4) },
+            Token::Seq { len: Some(6) },
             Token::Struct {
                 name: "StreamSource",
                 len: 1,
@@ -44,6 +42,25 @@ fn stream_source() {
             },
             Token::Str("ytId"),
             Token::Str("yt_id"),
+            Token::StructEnd,
+            Token::Struct {
+                name: "StreamSource",
+                len: 2,
+            },
+            Token::Str("infoHash"),
+            Token::Str("0101010101010101010101010101010101010101"),
+            Token::Str("fileIdx"),
+            Token::Some,
+            Token::U16(1),
+            Token::StructEnd,
+            Token::Struct {
+                name: "StreamSource",
+                len: 2,
+            },
+            Token::Str("infoHash"),
+            Token::Str("0101010101010101010101010101010101010101"),
+            Token::Str("fileIdx"),
+            Token::None,
             Token::StructEnd,
             Token::Struct {
                 name: "StreamSource",
@@ -62,36 +79,49 @@ fn stream_source() {
             Token::SeqEnd,
         ],
     );
-    assert_ser_tokens(
-        &torrents,
-        &[
-            Token::Seq { len: Some(2) },
-            Token::Struct {
-                name: "StreamSource",
-                len: 2,
-            },
-            Token::Str("infoHash"),
-            Token::Str("0101010101010101010101010101010101010101"),
-            Token::Str("fileIdx"),
-            Token::Some,
-            Token::U16(1),
-            Token::StructEnd,
-            Token::Struct {
-                name: "StreamSource",
-                len: 2,
-            },
-            Token::Str("infoHash"),
-            Token::Str("0101010101010101010101010101010101010101"),
-            Token::Str("fileIdx"),
-            Token::None,
-            Token::StructEnd,
-            Token::SeqEnd,
-        ],
-    );
     assert_de_tokens(
-        &torrents,
+        &vec![
+            StreamSource::Url {
+                url: Url::parse("https://url").unwrap(),
+            },
+            StreamSource::YouTube {
+                yt_id: "yt_id".to_owned(),
+            },
+            StreamSource::Torrent {
+                info_hash: [1; 20],
+                file_idx: Some(1),
+            },
+            StreamSource::Torrent {
+                info_hash: [1; 20],
+                file_idx: None,
+            },
+            StreamSource::Torrent {
+                info_hash: [1; 20],
+                file_idx: None,
+            },
+            StreamSource::External {
+                external_url: Url::parse("https://external_url").unwrap(),
+            },
+            StreamSource::PlayerFrame {
+                player_frame_url: Url::parse("https://player_frame_url").unwrap(),
+            },
+        ],
         &[
-            Token::Seq { len: Some(2) },
+            Token::Seq { len: Some(7) },
+            Token::Struct {
+                name: "StreamSource",
+                len: 1,
+            },
+            Token::Str("url"),
+            Token::Str("https://url/"),
+            Token::StructEnd,
+            Token::Struct {
+                name: "StreamSource",
+                len: 1,
+            },
+            Token::Str("ytId"),
+            Token::Str("yt_id"),
+            Token::StructEnd,
             Token::Struct {
                 name: "StreamSource",
                 len: 2,
@@ -110,6 +140,27 @@ fn stream_source() {
             Token::BorrowedBytes(b"0101010101010101010101010101010101010101"),
             Token::Str("fileIdx"),
             Token::None,
+            Token::StructEnd,
+            Token::Struct {
+                name: "StreamSource",
+                len: 2,
+            },
+            Token::Str("infoHash"),
+            Token::BorrowedBytes(b"0101010101010101010101010101010101010101"),
+            Token::StructEnd,
+            Token::Struct {
+                name: "StreamSource",
+                len: 1,
+            },
+            Token::Str("externalUrl"),
+            Token::Str("https://external_url/"),
+            Token::StructEnd,
+            Token::Struct {
+                name: "StreamSource",
+                len: 1,
+            },
+            Token::Str("playerFrameUrl"),
+            Token::Str("https://player_frame_url/"),
             Token::StructEnd,
             Token::SeqEnd,
         ],
