4e502b5b6d27ed99ff21a5c92580da9de785ee14
nklhrstv <me@nikolahristov.com>
2020-12-07 15:16:33 +0200

    Default derived for Subtitles in test env

--- src/types/resource/subtitles.rs
+++ src/types/resource/subtitles.rs
@@ -1,11 +1,18 @@
+#[cfg(test)]
+use derivative::Derivative;
 use serde::{Deserialize, Serialize};
 use url::Url;
 
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
-#[cfg_attr(test, derive(Debug))]
+#[cfg_attr(test, derive(Derivative, Debug))]
+#[cfg_attr(test, derivative(Default))]
 pub struct Subtitles {
     pub id: String,
     // @TODO: ISO 639-2
     pub lang: String,
+    #[cfg_attr(
+        test,
+        derivative(Default(value = "Url::parse(\"protocol://host\").unwrap()"))
+    )]
     pub url: Url,
 }
