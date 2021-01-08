99494d13a6082297ff08c7a5881aa723b08761d5
nklhrstv <me@nikolahristov.com>
2021-01-08 16:31:37 +0200

    flush events on authkey changed

--- src/analytics/analytics.rs
+++ src/analytics/analytics.rs
@@ -25,41 +25,57 @@ pub struct AnalyticsEvent {
 }
 
 pub struct AnalyticsState {
-    pub visit_id: String,
     pub auth_key: Option<AuthKey>,
     pub events: Vec<AnalyticsEvent>,
 }
 
 #[derive(Clone)]
 pub struct Analytics {
+    visit_id: String,
     state: Arc<RwLock<AnalyticsState>>,
 }
 
 impl Analytics {
     pub fn new(visit_id: String) -> Self {
         Self {
+            visit_id,
             state: Arc::new(RwLock::new(AnalyticsState {
-                visit_id,
                 auth_key: None,
                 events: vec![],
             })),
         }
     }
-    pub fn emit(&mut self, event: StremioEvent, _ctx: &Ctx<WebEnv>) {
+    pub fn emit(&self, event: StremioEvent, ctx: &Ctx<WebEnv>) {
+        let state = self.state.read().expect("analytics state read failed");
+        if state.auth_key.as_ref() != ctx.profile.auth_key() {
+            self.flush();
+        };
+        let mut state = self.state.write().expect("analytics state write failed");
+        state.auth_key = ctx.profile.auth_key().cloned();
+        let context = AnalyticsContext {
+            url: web_sys::window()
+                .expect("window is not available")
+                .location()
+                .href()
+                .expect("href is not available"),
+        };
         let event = match event {
             StremioEvent::CoreEvent(Event::UserAuthenticated { .. }) => AnalyticsEvent {
                 name: "login".to_owned(),
-                context: AnalyticsContext {
-                    url: web_sys::window()
-                        .expect("window is not available")
-                        .location()
-                        .href()
-                        .unwrap(),
-                },
+                context,
             },
             _ => return,
         };
+        state.events.push(event);
+    }
+    fn flush(&self) {
+        let mut state = self.state.write().expect("analytics state write failed");
+        let events = state.events.drain(..).collect::<Vec<_>>();
         #[cfg(debug_assertions)]
-        WebEnv::log(serde_json::to_string(&event).unwrap());
+        WebEnv::log(format!(
+            "AuthKey: {}, Events: {}",
+            serde_json::to_string(&state.auth_key).unwrap(),
+            serde_json::to_string(&events).unwrap()
+        ));
     }
 }
