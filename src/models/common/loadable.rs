da3ba4868ad4d9cf7a7d728981bce82bfe232566
nklhrstv <me@nikolahristov.com>
2021-01-19 17:32:20 +0200

    ready and err methods added to Loadable impl

--- src/models/common/loadable.rs
+++ src/models/common/loadable.rs
@@ -33,6 +33,20 @@ impl<R, E> Loadable<R, E> {
         }
     }
     #[inline]
+    pub fn ready(self) -> Option<R> {
+        match self {
+            Loadable::Ready(r) => Some(r),
+            _ => None,
+        }
+    }
+    #[inline]
+    pub fn err(self) -> Option<E> {
+        match self {
+            Loadable::Err(e) => Some(e),
+            _ => None,
+        }
+    }
+    #[inline]
     pub fn expect(self, msg: &str) -> R {
         match self {
             Self::Ready(r) => r,
