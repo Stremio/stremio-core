446f17252f5c2bc91991ef16fe4bc1f216249b50
Tim <tymmesyde@gmail.com>
2021-08-23 17:58:17 +0200

    implement magnet_url fn for Stream

--- src/types/resource/stream.rs
+++ src/types/resource/stream.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 use serde_hex::{SerHex, Strict};
 use std::collections::HashMap;
 use url::Url;
+use magnet_url::Magnet;
 
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
 #[cfg_attr(debug_assertions, derive(Debug))]
@@ -20,6 +21,32 @@ pub struct Stream {
     pub subtitles: Vec<Subtitles>,
     #[serde(default, skip_serializing_if = "is_default_value")]
     pub behavior_hints: StreamBehaviorHints,
+}
+
+impl Stream {
+    pub fn magnet_url(&self) -> Option<Magnet> {
+        match &self.source {
+            StreamSource::Torrent { info_hash, announce, .. } => {
+                Some(Magnet {
+                    dn: self.title.to_owned(),
+                    hash_type: Some("btih".to_string()),
+                    xt: Some(hex::encode(info_hash).to_string()),
+                    xl: Some(0),
+                    tr: announce
+                        .iter()
+                        .filter(|source| source.starts_with("tracker:"))
+                        .map(|tracker| tracker.replace("tracker:", ""))
+                        .collect::<Vec<String>>(),
+                    kt: Some(String::new()),
+                    ws: Some(String::new()),
+                    acceptable_source: Some(String::new()),
+                    mt: Some(String::new()),
+                    xs: Some(String::new()),
+                })
+            },
+            _ => None
+        }
+    }
 }
 
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
