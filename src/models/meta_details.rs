9b95ba81ce56eeac6ff44e9a924d6c211fe298bc
nklhrstv <me@nikolahristov.com>
2022-05-26 18:46:01 +0300

    watched field type changed

--- src/models/meta_details.rs
+++ src/models/meta_details.rs
@@ -7,9 +7,10 @@ use crate::models::ctx::Ctx;
 use crate::runtime::msg::{Action, ActionLoad, ActionMetaDetails, Internal, Msg};
 use crate::runtime::{Effects, Env, UpdateWithCtx};
 use crate::types::addon::{AggrRequest, ResourcePath};
-use crate::types::library::{LibraryBucket, LibraryItem, LibraryItemState};
+use crate::types::library::{LibraryBucket, LibraryItem};
 use crate::types::resource::{MetaItem, Stream};
 use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
 use stremio_watched_bitfield::WatchedBitField;
 
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
@@ -27,7 +28,7 @@ pub struct MetaDetails {
     pub streams: Vec<ResourceLoadable<Vec<Stream>>>,
     pub library_item: Option<LibraryItem>,
     #[serde(skip_serializing)]
-    pub watched: Vec<Option<WatchedBitField>>,
+    pub watched: Option<WatchedBitField>,
 }
 
 impl<E: Env + 'static> UpdateWithCtx<E> for MetaDetails {
@@ -62,8 +63,12 @@ impl<E: Env + 'static> UpdateWithCtx<E> for MetaDetails {
                     }
                     None => eq_update(&mut self.streams, vec![]),
                 };
-                let library_item_effects =
-                    library_item_update(&mut self.library_item, &self.selected, &ctx.library);
+                let library_item_effects = library_item_update::<E>(
+                    &mut self.library_item,
+                    &self.selected,
+                    &self.meta_items,
+                    &ctx.library,
+                );
                 let watched_effects =
                     watched_update::<E>(&mut self.watched, &self.meta_items, &self.library_item);
                 selected_effects
@@ -77,7 +82,7 @@ impl<E: Env + 'static> UpdateWithCtx<E> for MetaDetails {
                 let meta_items_effects = eq_update(&mut self.meta_items, vec![]);
                 let streams_effects = eq_update(&mut self.streams, vec![]);
                 let library_item_effects = eq_update(&mut self.library_item, None);
-                let watched_effects = eq_update(&mut self.watched, vec![]);
+                let watched_effects = eq_update(&mut self.watched, None);
                 selected_effects
                     .join(meta_items_effects)
                     .join(streams_effects)
@@ -87,26 +92,17 @@ impl<E: Env + 'static> UpdateWithCtx<E> for MetaDetails {
             Msg::Action(Action::MetaDetails(ActionMetaDetails::MarkAsWatched(
                 video_id,
                 is_watched,
-            ))) => {
-                let watched = self
-                    .meta_items
-                    .iter()
-                    .zip(self.watched.iter())
-                    .find(|(meta_item, _)| matches!(&meta_item.content, Some(Loadable::Ready(_))))
-                    .map(|(_, watched)| watched.as_ref())
-                    .flatten();
-                match (&self.library_item, watched) {
-                    (Some(library_item), Some(watched)) => {
-                        let mut library_item = library_item.to_owned();
-                        let mut watched = watched.to_owned();
-                        watched.set_video(video_id, *is_watched);
-                        library_item.state.watched = Some(watched.to_string());
-                        Effects::msg(Msg::Internal(Internal::UpdateLibraryItem(library_item)))
-                            .unchanged()
-                    }
-                    _ => Effects::none().unchanged(),
+            ))) => match (&self.library_item, &self.watched) {
+                (Some(library_item), Some(watched)) => {
+                    let mut watched = watched.to_owned();
+                    watched.set_video(video_id, *is_watched);
+                    let mut library_item = library_item.to_owned();
+                    library_item.state.watched = Some(watched.to_string());
+                    Effects::msg(Msg::Internal(Internal::UpdateLibraryItem(library_item)))
+                        .unchanged()
                 }
-            }
+                _ => Effects::none().unchanged(),
+            },
             Msg::Internal(Internal::ResourceRequestResult(request, result))
                 if request.path.resource == META_RESOURCE_NAME =>
             {
@@ -152,8 +148,12 @@ impl<E: Env + 'static> UpdateWithCtx<E> for MetaDetails {
                 )
             }
             Msg::Internal(Internal::LibraryChanged(_)) => {
-                let library_item_effects =
-                    library_item_update(&mut self.library_item, &self.selected, &ctx.library);
+                let library_item_effects = library_item_update::<E>(
+                    &mut self.library_item,
+                    &self.selected,
+                    &self.meta_items,
+                    &ctx.library,
+                );
                 let watched_effects =
                     watched_update::<E>(&mut self.watched, &self.meta_items, &self.library_item);
                 library_item_effects.join(watched_effects)
@@ -163,57 +163,73 @@ impl<E: Env + 'static> UpdateWithCtx<E> for MetaDetails {
     }
 }
 
-fn library_item_update(
+fn library_item_update<E: Env + 'static>(
     library_item: &mut Option<LibraryItem>,
     selected: &Option<Selected>,
+    meta_items: &[ResourceLoadable<MetaItem>],
     library: &LibraryBucket,
 ) -> Effects {
+    let meta_item = meta_items
+        .iter()
+        .find_map(|meta_item| match &meta_item.content {
+            Some(Loadable::Ready(meta_item)) => Some(meta_item),
+            _ => None,
+        });
     let next_library_item = match selected {
-        Some(selected) => library.items.get(&selected.meta_path.id).cloned(),
+        Some(selected) => library
+            .items
+            .get(&selected.meta_path.id)
+            .map(|library_item| {
+                meta_item.map_or_else(
+                    || library_item.to_owned(),
+                    |meta_item| LibraryItem::from((&meta_item.preview, library_item)),
+                )
+            })
+            .or_else(|| {
+                meta_item.map(|meta_item| LibraryItem::from((&meta_item.preview, PhantomData::<E>)))
+            }),
         _ => None,
     };
     eq_update(library_item, next_library_item)
 }
 
 fn watched_update<E: Env>(
-    watched: &mut Vec<Option<WatchedBitField>>,
+    watched: &mut Option<WatchedBitField>,
     meta_items: &[ResourceLoadable<MetaItem>],
     library_item: &Option<LibraryItem>,
 ) -> Effects {
     let next_watched = meta_items
         .iter()
-        .map(|meta_item| match &meta_item.content {
-            Some(Loadable::Ready(meta_item)) => {
-                let video_ids = meta_item
-                    .videos
-                    .iter()
-                    .map(|video| &video.id)
-                    .cloned()
-                    .collect::<Vec<_>>();
-                match library_item {
-                    Some(LibraryItem {
-                        state:
-                            LibraryItemState {
-                                watched: Some(watched),
-                                ..
-                            },
-                        ..
-                    }) => {
-                        match WatchedBitField::construct_and_resize(watched, video_ids.to_owned()) {
-                            Ok(watched) => Some(watched),
-                            Err(error) => {
-                                #[cfg(debug_assertions)]
-                                E::log(error.to_string());
-                                Some(WatchedBitField::construct_from_array(vec![], video_ids))
-                            }
-                        }
-                    }
-                    _ => Some(WatchedBitField::construct_from_array(vec![], video_ids)),
-                }
-            }
+        .find_map(|meta_item| match &meta_item.content {
+            Some(Loadable::Ready(meta_item)) => Some(meta_item),
             _ => None,
         })
-        .collect::<Vec<_>>();
+        .and_then(|meta_item| {
+            library_item
+                .as_ref()
+                .map(|library_item| (meta_item, &library_item.state.watched))
+        })
+        .map(|(meta_item, watched)| {
+            let video_ids = meta_item
+                .videos
+                .iter()
+                .map(|video| &video.id)
+                .cloned()
+                .collect::<Vec<_>>();
+            match watched {
+                Some(watched) => {
+                    match WatchedBitField::construct_and_resize(watched, video_ids.to_owned()) {
+                        Ok(watched) => watched,
+                        Err(error) => {
+                            #[cfg(debug_assertions)]
+                            E::log(error.to_string());
+                            WatchedBitField::construct_from_array(vec![], video_ids)
+                        }
+                    }
+                }
+                _ => WatchedBitField::construct_from_array(vec![], video_ids),
+            }
+        });
     eq_update(watched, next_watched)
 }
 
