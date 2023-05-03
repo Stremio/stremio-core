52e66c3b9dbffa72a8f5e0a4680bdb6289b660c6
unclekingpin <unclekingpin@proton.me>
2023-05-03 21:20:24 +0300

    send diff of transport_urls in AddonsPulledFromAPI

--- src/types/addon/descriptor.rs
+++ src/types/addon/descriptor.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use url::Url;
 
 /// Addon descriptor
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug)]
+#[derive(Clone, PartialEq, Eq, Hash, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Descriptor {
     pub manifest: Manifest,
@@ -19,7 +19,7 @@ pub struct DescriptorPreview {
     pub transport_url: Url,
 }
 
-#[derive(Default, Clone, PartialEq, Eq, Serialize, Deserialize, Debug)]
+#[derive(Default, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct DescriptorFlags {
     #[serde(default)]
