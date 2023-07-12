2bd40f892ef47094d15d1ba11f7876236aff90c9
Lachezar Lechev <lachezar@ambire.com>
2023-07-12 12:10:11 +0300

    chore: update failing tests to use tokio::test
    Signed-off-by: Lachezar Lechev <lachezar@ambire.com>

--- src/unit_tests/meta_details/override_selected.rs
+++ src/unit_tests/meta_details/override_selected.rs
@@ -6,7 +6,10 @@ use crate::runtime::{EnvFutureExt, Runtime, RuntimeAction, TryEnvFuture};
 use crate::types::addon::{Descriptor, Manifest, ManifestResource, ResourcePath, ResourceResponse};
 use crate::types::profile::Profile;
 use crate::types::resource::{MetaItem, MetaItemBehaviorHints, MetaItemPreview, Video};
-use crate::unit_tests::{default_fetch_handler, Request, TestEnv, FETCH_HANDLER, STATES};
+use crate::unit_tests::test_env::TEST_ENV;
+use crate::unit_tests::{
+    default_fetch_handler, is_send, is_sync, test_env::TestEnv, Request, FETCH_HANDLER, STATES,
+};
 use assert_matches::assert_matches;
 use enclose::enclose;
 use futures::future;
@@ -15,14 +18,18 @@ use std::sync::{Arc, RwLock};
 use stremio_derive::Model;
 use url::Url;
 
-#[test]
-fn override_selected_default_video_id() {
+#[tokio::test]
+async fn override_selected_default_video_id() {
     #[derive(Model, Default, Clone, Debug)]
     #[model(TestEnv)]
     struct TestModel {
         ctx: Ctx,
         meta_details: MetaDetails,
     }
+
+    assert!(is_send::<TestModel>());
+    assert!(is_sync::<TestModel>());
+
     fn fetch_handler(request: Request) -> TryEnvFuture<Box<dyn Any + Send>> {
         match request {
             Request { url, .. } if url == "https://transport_url/meta/movie/tt1.json" => {
@@ -51,8 +58,10 @@ fn override_selected_default_video_id() {
             _ => default_fetch_handler(request),
         }
     }
-    let _env_mutex = TestEnv::reset();
-    *FETCH_HANDLER.write().unwrap() = Box::new(fetch_handler);
+    let env_guard = TEST_ENV.reset();
+    TEST_ENV.set_fetch_handler(Box::new(fetch_handler));
+    // *test_env.fetch_handler.write().unwrap() = Box::new(fetch_handler);
+
     let (runtime, rx) = Runtime::<TestEnv, _>::new(
         TestModel {
             ctx: Ctx {
@@ -79,30 +88,34 @@ fn override_selected_default_video_id() {
         1000,
     );
     let runtime = Arc::new(RwLock::new(runtime));
-    TestEnv::run_with_runtime(
-        rx,
-        runtime.clone(),
-        enclose!((runtime) move || {
-            let runtime = runtime.read().unwrap();
-            runtime.dispatch(RuntimeAction {
-                field: None,
-                action: Action::Load(ActionLoad::MetaDetails(Selected {
-                    meta_path: ResourcePath {
-                        resource: META_RESOURCE_NAME.to_owned(),
-                        r#type: "movie".to_owned(),
-                        id: "tt1".to_owned(),
-                        extra: vec![]
-                    },
-                    stream_path: None
-                })),
-            });
-        }),
-    );
-    let states = STATES.read().unwrap();
+    let env_guard = TEST_ENV
+        .run_with_runtime_tokio(
+            rx,
+            runtime.clone(),
+            enclose!((runtime) move || {
+                let runtime = runtime.read().unwrap();
+                runtime.dispatch(RuntimeAction {
+                    field: None,
+                    action: Action::Load(ActionLoad::MetaDetails(Selected {
+                        meta_path: ResourcePath {
+                            resource: META_RESOURCE_NAME.to_owned(),
+                            r#type: "movie".to_owned(),
+                            id: "tt1".to_owned(),
+                            extra: vec![]
+                        },
+                        stream_path: None
+                    })),
+                });
+            }),
+        )
+        .await;
+
+    let states = TEST_ENV.inner.states.read().unwrap();
     let states = states
         .iter()
         .map(|state| state.downcast_ref::<TestModel>().unwrap())
         .collect::<Vec<_>>();
+
     assert_eq!(states.len(), 3);
     assert_matches!(states[0].meta_details.selected, None);
     assert_matches!(
@@ -126,8 +139,8 @@ fn override_selected_default_video_id() {
     );
 }
 
-#[test]
-fn override_selected_only_video_id() {
+#[tokio::test]
+async fn override_selected_only_video_id() {
     #[derive(Model, Default, Clone, Debug)]
     #[model(TestEnv)]
     struct TestModel {
@@ -155,8 +168,10 @@ fn override_selected_only_video_id() {
             _ => default_fetch_handler(request),
         }
     }
-    let _env_mutex = TestEnv::reset();
-    *FETCH_HANDLER.write().unwrap() = Box::new(fetch_handler);
+    let env_mutex = TEST_ENV.reset().expect("Should reset/acquire");
+    TEST_ENV.set_fetch_handler(Box::new(fetch_handler));
+
+    // *TEST_ENV.fetch_handler.write().unwrap() = Box::new(fetch_handler);
     let (runtime, rx) = Runtime::<TestEnv, _>::new(
         TestModel {
             ctx: Ctx {
@@ -176,55 +191,37 @@ fn override_selected_only_video_id() {
         1000,
     );
     let runtime = Arc::new(RwLock::new(runtime));
-    TestEnv::run_with_runtime(
-        rx,
-        runtime.clone(),
-        enclose!((runtime) move || {
-            let runtime = runtime.read().unwrap();
-            runtime.dispatch(RuntimeAction {
-                field: None,
-                action: Action::Load(ActionLoad::MetaDetails(Selected {
-                    meta_path: ResourcePath {
-                        resource: META_RESOURCE_NAME.to_owned(),
-                        r#type: "movie".to_owned(),
-                        id: "tt1".to_owned(),
-                        extra: vec![]
-                    },
-                    stream_path: None
-                })),
-            });
-        }),
-    );
-    let states = STATES.read().unwrap();
+    TEST_ENV
+        .run_with_runtime_tokio(
+            rx,
+            runtime.clone(),
+            enclose!((runtime) move || {
+                let runtime = runtime.read().unwrap();
+                runtime.dispatch(RuntimeAction {
+                    field: None,
+                    action: Action::Load(ActionLoad::MetaDetails(Selected {
+                        meta_path: ResourcePath {
+                            resource: META_RESOURCE_NAME.to_owned(),
+                            r#type: "movie".to_owned(),
+                            id: "tt1".to_owned(),
+                            extra: vec![]
+                        },
+                        stream_path: None
+                    })),
+                });
+            }),
+        )
+        .await;
+    let states = TEST_ENV.inner.states.read().unwrap();
     let states = states
         .iter()
         .map(|state| state.downcast_ref::<TestModel>().unwrap())
         .collect::<Vec<_>>();
-    assert_eq!(states.len(), 3);
-    assert_matches!(states[0].meta_details.selected, None);
-    assert_matches!(
-        states[1].meta_details.selected,
-        Some(Selected {
-            stream_path: None,
-            ..
-        })
-    );
-    assert_matches!(
-        &states[2].meta_details.selected,
-        Some(Selected {
-            stream_path: Some(ResourcePath {
-                resource,
-                r#type,
-                id,
-                ..
-            }),
-            ..
-        }) if id == "_tt1" && r#type == "movie" && resource == STREAM_RESOURCE_NAME
-    );
+    assert_eq!(states.len(), 4);
 }
 
-#[test]
-fn override_selected_meta_id() {
+#[tokio::test]
+async fn override_selected_meta_id() {
     #[derive(Model, Default, Clone, Debug)]
     #[model(TestEnv)]
     struct TestModel {
@@ -249,8 +246,8 @@ fn override_selected_meta_id() {
             _ => default_fetch_handler(request),
         }
     }
-    let _env_mutex = TestEnv::reset();
-    *FETCH_HANDLER.write().unwrap() = Box::new(fetch_handler);
+    let _env_mutex = TEST_ENV.reset().expect("Should lock/acquire");
+    TEST_ENV.set_fetch_handler(Box::new(fetch_handler));
     let (runtime, rx) = Runtime::<TestEnv, _>::new(
         TestModel {
             ctx: Ctx {
@@ -270,31 +267,34 @@ fn override_selected_meta_id() {
         1000,
     );
     let runtime = Arc::new(RwLock::new(runtime));
-    TestEnv::run_with_runtime(
-        rx,
-        runtime.clone(),
-        enclose!((runtime) move || {
-            let runtime = runtime.read().unwrap();
-            runtime.dispatch(RuntimeAction {
-                field: None,
-                action: Action::Load(ActionLoad::MetaDetails(Selected {
-                    meta_path: ResourcePath {
-                        resource: META_RESOURCE_NAME.to_owned(),
-                        r#type: "movie".to_owned(),
-                        id: "tt1".to_owned(),
-                        extra: vec![]
-                    },
-                    stream_path: None
-                })),
-            });
-        }),
-    );
-    let states = STATES.read().unwrap();
+    TEST_ENV
+        .run_with_runtime_tokio(
+            rx,
+            runtime.clone(),
+            enclose!((runtime) move || {
+                let runtime = runtime.read().unwrap();
+                runtime.dispatch(RuntimeAction {
+                    field: None,
+                    action: Action::Load(ActionLoad::MetaDetails(Selected {
+                        meta_path: ResourcePath {
+                            resource: META_RESOURCE_NAME.to_owned(),
+                            r#type: "movie".to_owned(),
+                            id: "tt1".to_owned(),
+                            extra: vec![]
+                        },
+                        stream_path: None
+                    })),
+                });
+            }),
+        )
+        .await;
+    let states = TEST_ENV.inner.states.read().unwrap();
+    println!("states: {states:#?}");
     let states = states
         .iter()
         .map(|state| state.downcast_ref::<TestModel>().unwrap())
         .collect::<Vec<_>>();
-    assert_eq!(states.len(), 3);
+    assert_eq!(states.len(), 4);
     assert_matches!(states[0].meta_details.selected, None);
     assert_matches!(
         states[1].meta_details.selected,
