70f2300ff1de62009d5c577ff83441067355388e
Lachezar Lechev <lachezar@ambire.com>
2023-10-17 16:20:14 +0300

    feat: add dev build and skip package.json creation
    Signed-off-by: Lachezar Lechev <lachezar@ambire.com>

--- scripts/dev-build.sh
+++ scripts/dev-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-wasm-pack build --no-typescript --no-pack --out-dir wasm_build --release --target web
+wasm-pack build --no-typescript --no-pack --out-dir wasm_build --dev --target web
 mv ./wasm_build/stremio_core_web_bg.wasm stremio_core_web_bg.wasm
 npx babel wasm_build/stremio_core_web.js --config-file ./.babelrc --out-file stremio_core_web.js
 npx babel src/bridge.js --config-file ./.babelrc --out-file bridge.js
