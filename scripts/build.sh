91e398a5b3566a9d976ce837882f1d940a032189
Lachezar Lechev <lachezar@ambire.com>
2023-10-24 11:16:30 +0300

    chore: Remove second build script and add argument
    Signed-off-by: Lachezar Lechev <lachezar@ambire.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
-wasm-pack build --no-typescript --no-pack --out-dir wasm_build --release --target web
+MODE=${1:---release}
+wasm-pack build --no-typescript --no-pack --out-dir wasm_build $MODE --target web
 mv ./wasm_build/stremio_core_web_bg.wasm stremio_core_web_bg.wasm
 npx babel wasm_build/stremio_core_web.js --config-file ./.babelrc --out-file stremio_core_web.js
 npx babel src/bridge.js --config-file ./.babelrc --out-file bridge.js
--- scripts/dev-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ex
-wasm-pack build --no-typescript --no-pack --out-dir wasm_build --dev --target web
-mv ./wasm_build/stremio_core_web_bg.wasm stremio_core_web_bg.wasm
-npx babel wasm_build/stremio_core_web.js --config-file ./.babelrc --out-file stremio_core_web.js
-npx babel src/bridge.js --config-file ./.babelrc --out-file bridge.js
-npx babel src/worker.js --config-file ./.babelrc --out-file worker.js
