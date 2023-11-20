62d358fc7a7b9a157c5be4c18e09944e5a751a7b
Tim <tymmesyde@gmail.com>
2023-11-20 13:27:08 +0100

    fix(search_history_bucket): remove unnecessary serde derive

--- src/types/search_history/search_history_bucket.rs
+++ src/types/search_history/search_history_bucket.rs
@@ -2,11 +2,9 @@ use std::collections::HashMap;
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use serde_with::serde_as;
 
 use crate::types::profile::UID;
 
-#[serde_as]
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SearchHistoryBucket {
     pub uid: UID,
