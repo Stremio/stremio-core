453c3d2cb7dd2eff1b8f73a95e171ba5c68b2a09
Tim <tymmesyde@gmail.com>
2023-12-14 13:10:38 +0100

    doc(types): add comment for dismissed_events_bucket
    Co-authored-by: Lachezar Lechev <8925621+elpiel@users.noreply.github.com>

--- src/types/events/dimissed_events_bucket.rs
+++ src/types/events/dimissed_events_bucket.rs
@@ -8,6 +8,8 @@ use crate::types::profile::UID;
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct DismissedEventsBucket {
     pub uid: UID,
+    /// the key is the event (modal or notification) that has been dismissed
+    /// while the value is the time at which it was dismissed.
     pub items: HashMap<String, DateTime<Local>>,
 }
 
