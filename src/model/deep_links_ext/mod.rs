8552e4960c360b4367d9580a8396fde265e8fcd7
Tim <tymmesyde@gmail.com>
2024-01-02 10:41:34 +0100

    feat: implement deep links ext for search history item

--- src/model/deep_links_ext/mod.rs
+++ src/model/deep_links_ext/mod.rs
@@ -6,5 +6,6 @@ mod discover_deep_links;
 mod library_deep_links;
 mod library_item_deep_links;
 mod meta_item_deep_links;
+mod search_history_deep_links;
 mod stream_deep_links;
 mod video_deep_links;
