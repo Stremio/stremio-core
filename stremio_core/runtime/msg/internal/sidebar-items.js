1a17f6a93f767233c5d425e2c2c55a5bef9ff49c
elpiel <elpiel@users.noreply.github.com>
2024-01-31 15:48:51 +0000

    deploy: 19ff6d61936debc6cc91581783044599933dbd5c

--- stremio_core/runtime/msg/internal/sidebar-items.js
+++ stremio_core/runtime/msg/internal/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Internal"],"type":["AuthResponse","CtxStorageResponse","LibraryPlanResponse"]};
+window.SIDEBAR_ITEMS = {"enum":["Internal"],"struct":["CtxAuthResponse"],"type":["CtxStorageResponse","LibraryPlanResponse"]};
