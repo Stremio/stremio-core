c9c18447a0ab006c230699205aebfd78f41c9902
Timothy Z. <timothy@stremio.com>
2024-10-09 15:11:23 +0300

    refactor: update variable name

--- src/models/ctx/update_streaming_server_urls.rs
+++ src/models/ctx/update_streaming_server_urls.rs
@@ -11,7 +11,7 @@ use futures::FutureExt;
 use super::{CtxError, CtxStatus};
 
 pub fn update_streaming_server_urls<E: Env + 'static>(
-    server_urls: &mut ServerUrlsBucket,
+    streaming_server_urls: &mut ServerUrlsBucket,
     status: &CtxStatus,
     msg: &Msg,
 ) -> Effects {
@@ -19,15 +19,15 @@ pub fn update_streaming_server_urls<E: Env + 'static>(
         Msg::Action(Action::StreamingServer(ActionStreamingServer::ServerUrlsBucket(action))) => {
             match action {
                 ActionServerUrlsBucket::AddServerUrl(url) => {
-                    server_urls.add_url(url.clone());
+                    streaming_server_urls.add_url(url.clone());
                     Effects::msg(Msg::Internal(Internal::StreamingServerUrlsBucketChanged))
                 }
                 ActionServerUrlsBucket::EditServerUrl { id, new_url } => {
-                    server_urls.edit_item(id, new_url.clone());
+                    streaming_server_urls.edit_item(id, new_url.clone());
                     Effects::msg(Msg::Internal(Internal::StreamingServerUrlsBucketChanged))
                 }
                 ActionServerUrlsBucket::DeleteServerUrl(id) => {
-                    server_urls.delete_item(id);
+                    streaming_server_urls.delete_item(id);
                     Effects::msg(Msg::Internal(Internal::StreamingServerUrlsBucketChanged))
                 }
             }
@@ -37,33 +37,36 @@ pub fn update_streaming_server_urls<E: Env + 'static>(
                 if loading_auth_request == auth_request =>
             {
                 let next_server_urls = ServerUrlsBucket::new(Some(auth.user.id.to_owned()));
-                *server_urls = next_server_urls;
+                *streaming_server_urls = next_server_urls;
                 Effects::msg(Msg::Internal(Internal::StreamingServerUrlsBucketChanged))
             }
             _ => Effects::none().unchanged(),
         },
         Msg::Internal(Internal::StreamingServerUrlsBucketChanged) => {
-            Effects::one(push_server_urls_to_storage::<E>(server_urls)).unchanged()
+            Effects::one(push_server_urls_to_storage::<E>(streaming_server_urls)).unchanged()
         }
         _ => Effects::none().unchanged(),
     }
 }
 
-fn push_server_urls_to_storage<E: Env + 'static>(server_urls: &ServerUrlsBucket) -> Effect {
-    let uid: Option<String> = server_urls.uid.clone();
+fn push_server_urls_to_storage<E: Env + 'static>(
+    streaming_server_urls: &ServerUrlsBucket,
+) -> Effect {
+    let uid: Option<String> = streaming_server_urls.uid.clone();
 
     EffectFuture::Sequential(
-        E::set_storage(STREAMING_SERVER_URLS_STORAGE_KEY, Some(server_urls))
-            .map(move |result| match result {
-                Ok(_) => Msg::Event(Event::StreamingServerUrlsPushedToStorage { uid: uid.clone() }),
-                Err(error) => Msg::Event(Event::Error {
-                    error: CtxError::from(error),
-                    source: Box::new(Event::StreamingServerUrlsPushedToStorage {
-                        uid: uid.clone(),
-                    }),
-                }),
-            })
-            .boxed_env(),
+        E::set_storage(
+            STREAMING_SERVER_URLS_STORAGE_KEY,
+            Some(streaming_server_urls),
+        )
+        .map(move |result| match result {
+            Ok(_) => Msg::Event(Event::StreamingServerUrlsPushedToStorage { uid: uid.clone() }),
+            Err(error) => Msg::Event(Event::Error {
+                error: CtxError::from(error),
+                source: Box::new(Event::StreamingServerUrlsPushedToStorage { uid: uid.clone() }),
+            }),
+        })
+        .boxed_env(),
     )
     .into()
 }
