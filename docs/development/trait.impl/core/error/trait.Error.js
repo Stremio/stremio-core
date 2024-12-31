7cb6bceb5edba5bc7bf11aa801dd6ae8bd0f4909
tymmesyde <tymmesyde@users.noreply.github.com>
2024-12-31 10:01:05 +0000

    deploy: 9e8d555e614059516e8b71d75aa12310a6a00805

--- docs/development/trait.impl/core/error/trait.Error.js
+++ docs/development/trait.impl/core/error/trait.Error.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["stremio_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"stremio_core/models/local_search/imdb_rating/enum.ParseError.html\" title=\"enum stremio_core::models::local_search::imdb_rating::ParseError\">ParseError</a>"]]],["stremio_watched_bitfield",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"stremio_watched_bitfield/error/struct.Error.html\" title=\"struct stremio_watched_bitfield::error::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["stremio_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"stremio_core/models/local_search/enum.ParseError.html\" title=\"enum stremio_core::models::local_search::ParseError\">ParseError</a>"]]],["stremio_watched_bitfield",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"stremio_watched_bitfield/struct.Error.html\" title=\"struct stremio_watched_bitfield::Error\">Error</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[349,323]}
+//{"start":57,"fragment_lengths":[324,310]}
--- docs/development/trait.impl/core/ops/deref/trait.Deref.js
+++ docs/development/trait.impl/core/ops/deref/trait.Deref.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["stremio_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"stremio_core/models/library_with_filters/struct.LibraryRequestPage.html\" title=\"struct stremio_core::models::library_with_filters::LibraryRequestPage\">LibraryRequestPage</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"stremio_core/types/addon/manifest/struct.OptionsLimit.html\" title=\"struct stremio_core::types::addon::manifest::OptionsLimit\">OptionsLimit</a>"]]]]);
+    var implementors = Object.fromEntries([["stremio_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"stremio_core/models/library_with_filters/struct.LibraryRequestPage.html\" title=\"struct stremio_core::models::library_with_filters::LibraryRequestPage\">LibraryRequestPage</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"stremio_core/types/addon/struct.OptionsLimit.html\" title=\"struct stremio_core::types::addon::OptionsLimit\">OptionsLimit</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[709]}
+//{"start":57,"fragment_lengths":[690]}
