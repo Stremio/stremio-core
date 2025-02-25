fba01b3f8d6cb804c42bb98cbb6a47d9217dcac1
tymmesyde <tymmesyde@users.noreply.github.com>
2025-02-25 14:08:39 +0000

    deploy: 1574e4a4ee40458ff48fb7939ce904ac956b7e0c

--- docs/development/stremio_core_web/model/serialize_catalogs_with_extra/sidebar-items.js
+++ docs/development/stremio_core_web/model/serialize_catalogs_with_extra/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Loadable","PosterShape"],"struct":["CatalogsWithExtra","Ctx","DescriptorPreview","DiscoverDeepLinks","ManifestPreview","MetaItemDeepLinks","MetaItemPreview","ResourceLoadable","Selected"]};
+window.SIDEBAR_ITEMS = {"enum":["Loadable","PosterShape"],"struct":["CatalogsWithExtra","Ctx","DescriptorPreview","DiscoverDeepLinks","ManifestPreview","MetaItemDeepLinks","MetaItemPreview","ResourceLoadable","Selected","Stream","StreamDeepLinks"]};
