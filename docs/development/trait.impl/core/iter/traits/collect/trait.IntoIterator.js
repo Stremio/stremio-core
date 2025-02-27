7834172330865fc4d57680bf946e61c80e326556
elpiel <elpiel@users.noreply.github.com>
2025-02-27 11:09:34 +0000

    deploy: 0ece89f570113872ebc17d8c872b8e0eec1a969c

--- docs/development/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
+++ docs/development/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["stremio_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"stremio_core/runtime/struct.Effects.html\" title=\"struct stremio_core::runtime::Effects\">Effects</a>"]]]]);
+    var implementors = Object.fromEntries([["stremio_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"stremio_core/runtime/struct.Effects.html\" title=\"struct stremio_core::runtime::Effects\">Effects</a><div class=\"where\">where\n    <a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"enum\" href=\"stremio_core/runtime/enum.Effect.html\" title=\"enum stremio_core::runtime::Effect\">Effect</a>&gt;: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[347]}
+//{"start":57,"fragment_lengths":[830]}
---
+++ docs/development/trait.impl/core/iter/traits/marker/trait.FusedIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["stremio_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"stremio_core/models/library_with_filters/struct.SortIter.html\" title=\"struct stremio_core::models::library_with_filters::SortIter\">SortIter</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[392]}
