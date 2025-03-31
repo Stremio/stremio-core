71002cc2e55c7f95c46d2f0face767d125606b65
Timothy Z. <timothy@stremio.com>
2025-03-31 10:57:17 +0300

    Revert "test: use test api url"
    This reverts commit c964ce79708ee3e0f9f1720b2b2ad352adf99aa6.

--- src/constants.rs
+++ src/constants.rs
@@ -76,7 +76,7 @@ pub static CINEMETA_URL: Lazy<Url> = Lazy::new(|| {
     Url::parse("https://v3-cinemeta.strem.io/manifest.json").expect("CINEMETA_URL parse failed")
 });
 pub static API_URL: Lazy<Url> =
-    Lazy::new(|| Url::parse("http://localhost:3009/").expect("API_URL parse failed"));
+    Lazy::new(|| Url::parse("https://api.strem.io").expect("API_URL parse failed"));
 pub static LINK_API_URL: Lazy<Url> =
     Lazy::new(|| Url::parse("https://link.stremio.com").expect("LINK_API_URL parse failed"));
 pub static STREAMING_SERVER_URL: Lazy<Url> =
