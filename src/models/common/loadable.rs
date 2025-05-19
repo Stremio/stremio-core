28e8979aaa2142faa53164a16e049a36c3b4db95
Lachezar Lechev <lachezar@ambire.com>
2025-05-19 19:54:34 +0300

    chore(core): loadable - remove duplicate impl for Result
    Signed-off-by: Lachezar Lechev <lachezar@ambire.com>

--- src/models/common/loadable.rs
+++ src/models/common/loadable.rs
@@ -92,12 +92,3 @@ impl<R, E> From<Result<R, E>> for Loadable<R, E> {
         }
     }
 }
-
-impl<R,E> From<Result<R, E>> for Loadable<R, E> {
-    fn from(result: Result<R, E>) -> Self {
-        match result {
-            Ok(x) => Loadable::Ready(x),
-            Err(err) => Loadable::Err(err),
-        }
-    }
-}
