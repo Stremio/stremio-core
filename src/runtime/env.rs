bed86df7cfa98ed1a799d22bcee6460bc0d4a356
Tim <tymmesyde@gmail.com>
2025-11-21 01:53:27 +0100

    feat: add ass_subtitles_styling setting

--- src/runtime/env.rs
+++ src/runtime/env.rs
@@ -301,6 +301,12 @@ pub trait Env {
                         .await?;
                     schema_version = 20;
                 }
+                if schema_version == 20 {
+                    migrate_storage_schema_to_v21::<Self>()
+                        .map_err(|error| EnvError::StorageSchemaVersionUpgrade(Box::new(error)))
+                        .await?;
+                    schema_version = 21;
+                }
                 if schema_version != SCHEMA_VERSION {
                     panic!(
                         "Storage schema version must be upgraded from {} to {}",
@@ -740,6 +746,29 @@ fn migrate_storage_schema_to_v20<E: Env>() -> TryEnvFuture<()> {
         .boxed_env()
 }
 
+fn migrate_storage_schema_to_v21<E: Env>() -> TryEnvFuture<()> {
+    E::get_storage::<serde_json::Value>(PROFILE_STORAGE_KEY)
+        .and_then(|mut profile| {
+            match profile
+                .as_mut()
+                .and_then(|profile| profile.as_object_mut())
+                .and_then(|profile| profile.get_mut("settings"))
+                .and_then(|settings| settings.as_object_mut())
+            {
+                Some(settings) => {
+                    settings.insert(
+                        "assSubtitlesStyling".to_owned(),
+                        serde_json::Value::Bool(true),
+                    );
+                    E::set_storage(PROFILE_STORAGE_KEY, Some(&profile))
+                }
+                _ => E::set_storage::<()>(PROFILE_STORAGE_KEY, None),
+            }
+        })
+        .and_then(|_| E::set_storage(SCHEMA_VERSION_STORAGE_KEY, Some(&21)))
+        .boxed_env()
+}
+
 #[cfg(test)]
 mod test {
     use serde_json::{json, Value};
@@ -755,9 +784,9 @@ mod test {
                 migrate_storage_schema_to_v14, migrate_storage_schema_to_v15,
                 migrate_storage_schema_to_v16, migrate_storage_schema_to_v17,
                 migrate_storage_schema_to_v18, migrate_storage_schema_to_v19,
-                migrate_storage_schema_to_v20, migrate_storage_schema_to_v6,
-                migrate_storage_schema_to_v7, migrate_storage_schema_to_v8,
-                migrate_storage_schema_to_v9,
+                migrate_storage_schema_to_v20, migrate_storage_schema_to_v21,
+                migrate_storage_schema_to_v6, migrate_storage_schema_to_v7,
+                migrate_storage_schema_to_v8, migrate_storage_schema_to_v9,
             },
             Env,
         },
@@ -1476,6 +1505,47 @@ mod test {
 
             assert_eq!(
                 &20.to_string(),
+                storage
+                    .get(SCHEMA_VERSION_STORAGE_KEY)
+                    .expect("Should have the schema set"),
+                "Scheme version should now be updated"
+            );
+            assert_eq!(
+                &migrated_profile.to_string(),
+                storage
+                    .get(PROFILE_STORAGE_KEY)
+                    .expect("Should have the profile set"),
+                "Profile should match"
+            );
+        }
+    }
+
+    #[tokio::test]
+    async fn test_migration_from_20_to_21() {
+        {
+            let _test_env_guard = TestEnv::reset().expect("Should lock TestEnv");
+            let profile_before = json!({
+                "settings": {}
+            });
+
+            let migrated_profile = json!({
+                "settings": {
+                    "assSubtitlesStyling": true,
+                }
+            });
+
+            // setup storage for migration
+            set_profile_and_schema_version(&profile_before, 20);
+
+            // migrate storage
+            migrate_storage_schema_to_v21::<TestEnv>()
+                .await
+                .expect("Should migrate");
+
+            let storage = STORAGE.read().expect("Should lock");
+
+            assert_eq!(
+                &21.to_string(),
                 storage
                     .get(SCHEMA_VERSION_STORAGE_KEY)
                     .expect("Should have the schema set"),
